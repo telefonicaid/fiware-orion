df28651d65980a676c9a46054fd55891cf8dbea4
Ken Zangelin <kzangeli@tid.es>
2013-10-30 22:50:22 +0100

    coverage at 100% for discovery

---
+++ test/unittests/jsonParse/jsonRequest_test.cpp
@@ -0,0 +1,56 @@
+/*
+*
+* Copyright 2013 Telefonica Investigacion y Desarrollo, S.A.U
+*
+* This file is part of Orion Context Broker.
+*
+* Orion Context Broker is free software: you can redistribute it and/or
+* modify it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* Orion Context Broker is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+* General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Orion Context Broker. If not, see http://www.gnu.org/licenses/.
+*
+* For those usages not covered by this license please contact with
+* fermin at tid dot es
+*
+* Author: Ken Zangelin
+*/
+#include "gtest/gtest.h"
+
+#include "logMsg/logMsg.h"
+#include "logMsg/traceLevels.h"
+
+#include "jsonParse/jsonRequest.h"
+#include "ngsi/ParseData.h"
+#include "ngsi/Request.h"
+
+
+
+/* ****************************************************************************
+*
+* - 
+*/
+TEST(jsonRequest, jsonTreat)
+{
+   ConnectionInfo  ci("/ngsi9/registerContext", "POST", "1.1");
+   ParseData       parseData;
+   
+   ci.outFormat = JSON;
+
+   std::string  out;
+   std::string  expected1 = "\"errorCode\" : {\n  \"code\" : \"400\",\n  \"reasonPhrase\" : \"no request treating object found\",\n  \"details\" : \"Sorry, no request treating object found for RequestType 'InvalidRequest'\"\n}\n";
+   std::string  expected2 = "\"errorCode\" : {\n  \"code\" : \"400\",\n  \"reasonPhrase\" : \"Sorry, not implemented\"\n}\n";
+
+   out  = jsonTreat("", &ci, &parseData, InvalidRequest, "no_payload", NULL);
+   EXPECT_EQ(expected1, out);
+
+   out = jsonTreat("", &ci, &parseData, UnsubscribeContext, "no_payload", NULL);
+   EXPECT_EQ(expected2, out);
+}
