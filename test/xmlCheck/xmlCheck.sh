6f15970029d7217a9893ed8f36160c1e3271ccb9
Ken Zangelin <kzangeli@tid.es>
2013-11-18 15:29:09 +0100

    exit codes to reflect outcome of XML check

--- test/xmlCheck/xmlCheck.sh
+++ test/xmlCheck/xmlCheck.sh
@@ -77,7 +77,7 @@ function xsdGet()
     echo $xsdDir/Ngsi10_Operations_v07.xsd
   else
     echo "unknown file prefix: '"${prefix}"' for $xfile"
-    exit 1
+    exit 2
   fi
 }
 
@@ -225,7 +225,7 @@ vMsg Git repo home: $SRC_TOP
 if [ ! -d "$xsdDir" ]
 then
   echo "$0: error: '"${xsdDir}"': no such directory"
-  exit 2
+  exit 3
 fi
 
 
@@ -256,7 +256,7 @@ then
   if [ ! -f Ngsi10_Operations_v07.xsd ] || [ ! -f Ngsi9_Operations_v07.xsd ] || [ ! -f Ngsi9_10_dataStructure_v07.xsd ]
   then
     echo $0: error: wget failed to download latest XSD files
-    exit 5
+    exit 4
   fi
 
   mv Ngsi10_Operations_v07.xsd Ngsi9_Operations_v07.xsd Ngsi9_10_dataStructure_v07.xsd $xsdDir
@@ -273,7 +273,7 @@ fi
 if [ ! -f $xsdDir/Ngsi10_Operations_v07.xsd ] || [ ! -f $xsdDir/Ngsi9_Operations_v07.xsd ] || [ ! -f $xsdDir/Ngsi9_10_dataStructure_v07.xsd ]
 then
   echo "$0: error: XSD files missing in $xsdDir"
-  exit 3
+  exit 5
 fi
 
 
@@ -460,7 +460,12 @@ fi
 #
 if [ "$file" != "" ]
 then
-  exit 0
+  if [ "$ERR" != "0"
+  then
+    exit 6
+  else
+    exit 0
+  fi
 fi
 
 
@@ -488,6 +493,8 @@ fi
 echo "Tested ${xmlDocsProcessed} (${xmlFilesProcessed} files + ${xmlPartsProcessed} parts) out of ${xmlDocsFound} (${xmlFilesFound} files + ${xmlPartsFound} parts) XML documents:"
 echo "  ${xmlDocsOk} documents passed the XML validity test"
 
+exitCode=8
+
 if [ "$xmlDocsErrors" != 0 ]
 then
   echo "  ${xmlDocsErrors} documents did not pass"
@@ -495,6 +502,7 @@ else
   echo "  -----------------------"
   echo "  ALL documents passed!!!"
   echo "  -----------------------"
+  exitCode=0
 fi
 
 echo
@@ -509,4 +517,7 @@ then
   do
     echo "  o $xfile"
   done
+  exit 7
 fi
+
+exit $exitCode
