29905bce88e63933bc2b1eeb19d59ff1253e7f2b
Ken Zangelin <kzangeli@tid.es>
2013-12-05 12:45:05 +0100

    more unit tests

--- test/unittests/serviceRoutines/getAttributeValueInstance_test.cpp
+++ test/unittests/serviceRoutines/getAttributeValueInstance_test.cpp
@@ -60,22 +60,16 @@ static RestService rs[] =
 *
 * notFound - 
 */
-TEST(putAttributeValueInstance, notFound)
+TEST(getAttributeValueInstance, notFound)
 {
-  ConnectionInfo ci("/ngsi10/contextEntities/E1/attributes/A1/left",  "PUT", "1.1");
-  std::string    expected   = "<statusCode>\n  <code>200</code>\n  <reasonPhrase>OK</reasonPhrase>\n</statusCode>\n";
-  std::string    want       = "<statusCode>\n  <code>404</code>\n  <reasonPhrase>Entity Not Found</reasonPhrase>\n  <details>E1</details>\n</statusCode>\n";
-  const char*    fileName   = "ngsi10.putAttributeValueInstance.A1-left.ok.postponed.xml";
+  ConnectionInfo ci("/ngsi10/contextEntities/E1/attributes/A1/left",  "GET", "1.1");
+  std::string    expected    = "<contextAttributeResponse>\n  <statusCode>\n    <code>404</code>\n    <reasonPhrase>The ContextElement requested is not found</reasonPhrase>\n    <details>E1-A1</details>\n  </statusCode>\n</contextAttributeResponse>\n";
   std::string    out;
 
   utInit();
   
-  EXPECT_EQ("OK", testDataFromFile(testBuf, sizeof(testBuf), fileName)) << "Error getting test data from '" << fileName << "'";
-  ci.outFormat    = XML;
-  ci.inFormat     = XML;
-  ci.payload      = testBuf;
-  ci.payloadSize  = strlen(testBuf);
-  out             = restService(&ci, rs);
+  ci.outFormat = XML;
+  out          = restService(&ci, rs);
   EXPECT_EQ(expected, out);
 
   utExit();
@@ -87,19 +81,18 @@ TEST(putAttributeValueInstance, notFound)
 *
 * found - 
 */
-TEST(putAttributeValueInstance, found)
+TEST(getAttributeValueInstance, found)
 {
   ConnectionInfo ci1("/ngsi10/contextEntities/E1/attributes",          "POST", "1.1");
-  ConnectionInfo ci2("/ngsi10/contextEntities/E1/attributes/A1/left",  "PUT", "1.1");
+  ConnectionInfo ci2("/ngsi10/contextEntities/E1/attributes/A1/left",  "GET", "1.1");
   std::string    expected1    = "<appendContextElementResponse>\n  <contextResponseList>\n    <contextAttributeResponse>\n      <contextAttributeList>\n        <contextAttribute>\n          <name>A1</name>\n          <type></type>\n          <contextValue></contextValue>\n          <metadata>\n            <contextMetadata>\n              <name>ID</name>\n              <type>string</type>\n              <value>left</value>\n            </contextMetadata>\n          </metadata>\n        </contextAttribute>\n      </contextAttributeList>\n      <statusCode>\n        <code>200</code>\n        <reasonPhrase>OK</reasonPhrase>\n      </statusCode>\n    </contextAttributeResponse>\n  </contextResponseList>\n</appendContextElementResponse>\n";
-  std::string    expected2    = "<statusCode>\n  <code>200</code>\n  <reasonPhrase>OK</reasonPhrase>\n</statusCode>\n";
-  const char*    fileName1    = "ngsi10.IndividualContextEntityAttributes.A1-left.postponed.xml";
-  const char*    fileName2    = "ngsi10.putAttributeValueInstance.A1-left.ok.postponed.xml";
+  std::string    expected2    = "<contextAttributeResponse>\n  <contextAttributeList>\n    <contextAttribute>\n      <name>A1</name>\n      <type></type>\n      <contextValue>10</contextValue>\n      <metadata>\n        <contextMetadata>\n          <name>ID</name>\n          <type>string</type>\n          <value>left</value>\n        </contextMetadata>\n      </metadata>\n    </contextAttribute>\n  </contextAttributeList>\n  <statusCode>\n    <code>200</code>\n    <reasonPhrase>OK</reasonPhrase>\n  </statusCode>\n</contextAttributeResponse>\n";
+  const char*    fileName     = "ngsi10.IndividualContextEntityAttributes.A1-left.postponed.xml";
   std::string    out;
 
   utInit();
 
-  EXPECT_EQ("OK", testDataFromFile(testBuf, sizeof(testBuf), fileName1)) << "Error getting test data from '" << fileName1 << "'";
+  EXPECT_EQ("OK", testDataFromFile(testBuf, sizeof(testBuf), fileName)) << "Error getting test data from '" << fileName << "'";
   ci1.outFormat    = XML;
   ci1.inFormat     = XML;
   ci1.payload      = testBuf;
@@ -107,13 +100,9 @@ TEST(putAttributeValueInstance, found)
   out              = restService(&ci1, rs);
   EXPECT_EQ(expected1, out);
 
-  EXPECT_EQ("OK", testDataFromFile(testBuf, sizeof(testBuf), fileName2)) << "Error getting test data from '" << fileName2 << "'";
   ci2.outFormat    = XML;
-  ci2.inFormat     = XML;
-  ci2.payload      = testBuf;
-  ci2.payloadSize  = strlen(testBuf);
   out              = restService(&ci2, rs);
-  EXPECT_EQ(expected2, out);
+  EXPECT_EQ(expected2, out);  
 
   utExit();
 }
