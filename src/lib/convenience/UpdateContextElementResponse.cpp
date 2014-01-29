fa83de98951eda013f9b29220226abb2fd257af0
LeandroGuillen <leandroguillen@gmail.com>
2014-01-29 14:18:01 +0100

    FIX unwanted tag in message

--- src/lib/convenience/UpdateContextElementResponse.cpp
+++ src/lib/convenience/UpdateContextElementResponse.cpp
@@ -42,7 +42,7 @@ std::string UpdateContextElementResponse::render(RequestType requestType, Format
    std::string tag = "updateContextElementResponse";
    std::string out = "";
 
-   out += startTag(indent, tag, format);
+   out += startTag(indent, tag, format, false);
 
    if ((errorCode.code != NO_ERROR_CODE) && (errorCode.code != SccOk))
      out += errorCode.render(format, indent + "  ");
