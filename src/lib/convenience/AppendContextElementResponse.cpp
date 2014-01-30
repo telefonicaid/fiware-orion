9d9efe7b029b5eca2570fbb61bff154814a95982
LeandroGuillen <leandroguillen@gmail.com>
2014-01-30 10:49:31 +0100

    FIX rendering bug

--- src/lib/convenience/AppendContextElementResponse.cpp
+++ src/lib/convenience/AppendContextElementResponse.cpp
@@ -44,7 +44,7 @@ std::string AppendContextElementResponse::render(RequestType requestType, Format
   std::string tag = "appendContextElementResponse";
   std::string out = "";
 
-  out += startTag(indent, tag, format);
+  out += startTag(indent, tag, format, false);
 
   if ((errorCode.code != NO_ERROR_CODE) && (errorCode.code != SccOk))
     out += errorCode.render(format, indent + "  ");
