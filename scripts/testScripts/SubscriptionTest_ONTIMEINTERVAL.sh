14cb935fb8482a6f416788340fbca6d679e77c98
xavierval <xval@hi-iberia.es>
2014-04-03 13:27:14 +0200

    SubscriptionTest_ONCHANGE typo

--- scripts/testScripts/SubscriptionTest_ONTIMEINTERVAL.sh
+++ scripts/testScripts/SubscriptionTest_ONTIMEINTERVAL.sh
@@ -1,14 +1,33 @@
-		
-# v0.2
-# USAGE:  ./subscriptTest_.sh endpoint:port amountOfSuscriptions updateTime notificationEndpoint
+ # Copyright 2013 Telefonica Investigacion y Desarrollo, S.A.U
+ #
+ # This file is part of Orion Context Broker.
+ #
+ # Orion Context Broker is free software: you can redistribute it and/or
+ # modify it under the terms of the GNU Affero General Public License as
+ # published by the Free Software Foundation, either version 3 of the
+ # License, or (at your option) any later version.
+ #
+ # Orion Context Broker is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ # General Public License for more details.
+ #
+ # You should have received a copy of the GNU Affero General Public License
+ # along with Orion Context Broker. If not, see http://www.gnu.org/licenses/.
+ #
+ # For those usages not covered by this license please contact with
+ # fermin at tid dot es
+
+# v0.3
+# USAGE:  ./subscriptionTest_ONx.sh endpoint:port amountOfSubscriptions updateTime notificationEndpoint
 # EXAMPLE ./subscriptionTest_ONx.sh 127.0.0.1:1026 60 60 http://127.0.0.1:1028/accumulate
 
 # START
-echo "#### Suscription ONTIMEINTERVAL test STARTED! #### "
+echo "#### Subscription ONTIMEINTERVAL test STARTED! #### "
 echo "Date: " $(date +"%m-%d-%Y %H:%M"); 
 
 
-# Default config: 60 suscriptions (delayed by 1 sec) and updated on TIMEINTERVAL 60s and sent to http://127.0.0.1:1028/accumulator server
+# Default config: 60 subscriptions (delayed by 1 sec) and updated on TIMEINTERVAL 60s and sent to http://127.0.0.1:1028/accumulator server
 #config
 n=0
 max=10
