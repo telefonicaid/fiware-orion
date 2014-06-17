21d9d5747b94f352696a56e73753a36cc0a66b77
Fermín Galán Márquez <fermin@tid.es>
2014-06-17 10:19:22 +0200

    FIX typo in comment

--- src/lib/mongoBackend/MongoGlobal.cpp
+++ src/lib/mongoBackend/MongoGlobal.cpp
@@ -1255,7 +1255,7 @@ bool processOnChangeCondition(EntityIdVector enV, AttributeList attrL, Condition
     NotifyContextRequest ncr;
 
     // FIXME P10: we are using dummy scope by the moment, until subscription scopes get implemented
-    // FIXME P10: we are using an empty service path vector until serive paths get implemented for subscriptions
+    // FIXME P10: we are using an empty service path vector until service paths get implemented for subscriptions
     std::vector<std::string> servicePathV;
     Restriction res;    
     if (!entitiesQuery(enV, attrL, res, &ncr.contextElementResponseVector, &err, false, tenant, servicePathV)) {
