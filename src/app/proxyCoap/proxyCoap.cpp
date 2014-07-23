f25b93b1bd76c50a3372fe31d922b65223122708
LeandroGuillen <LeandroGuillen@gmail.com>
2014-07-23 11:50:12 +0200

    FIX proxyCoap won't hang when his intended UDP port is in use

--- src/app/proxyCoap/proxyCoap.cpp
+++ src/app/proxyCoap/proxyCoap.cpp
@@ -196,10 +196,13 @@ int main(int argC, char* argV[])
   if (fg == false)
     daemonize();
 
-  boost::thread *coapServerThread = new boost::thread(boost::bind(&CoapController::serve, new CoapController(bindAddress, cbPort, port)));
-  coapServerThread->get_id(); // to prevent 'warning: unused'
+  CoapController* cc = new CoapController(bindAddress, cbPort, port);
+  cc->serve();
 
-  while (1)
-    sleep(10);
+//  boost::thread *coapServerThread = new boost::thread(boost::bind(&CoapController::serve, new CoapController(bindAddress, cbPort, port)));
+//  coapServerThread->get_id(); // to prevent 'warning: unused'
+
+//  while (1)
+//    sleep(10);
 }
 
