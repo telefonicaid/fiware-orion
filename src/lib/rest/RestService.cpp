423633994974aa0216357565707e1f15a2e7df5b
Ken Zangelin <kzangeli@tid.es>
2014-10-22 11:39:34 +0200

    Fixed CHANGES, and added {} to RestService.cpp

--- src/lib/rest/RestService.cpp
+++ src/lib/rest/RestService.cpp
@@ -132,9 +132,13 @@ static void commonFilters
     // Lookup the restriction of the correct parseData, where to add the new scope.
     //
     if (serviceP->request == EntityTypes)
+    {
       restrictionP = &parseDataP->qcr.res.restriction;
+    }
     else if (serviceP->request == AllContextEntities)
+    {
       restrictionP = &parseDataP->qcr.res.restriction;
+    }
 
     if (restrictionP == NULL)
     {
@@ -165,9 +169,13 @@ static void commonFilters
     // Lookup the restriction of the correct parseData, where to add the new scope.
     //
     if (serviceP->request == EntityTypes)
+    {
       restrictionP = &parseDataP->qcr.res.restriction;
+    }
     else if (serviceP->request == AllContextEntities)
+    {
       restrictionP = &parseDataP->qcr.res.restriction;
+    }
 
     if (restrictionP == NULL)
     {
@@ -204,8 +212,10 @@ std::string restService(ConnectionInfo* ciP, RestService* serviceV)
   {
     OrionError  error(SccBadRequest, "The Orion Context Broker is a REST service, not a 'web page'");
     std::string response = error.render(ciP->outFormat, "");
+
     LM_W(("Bad Input (The Orion Context Broker is a REST service, not a 'web page')"));
     restReply(ciP, response);
+
     return std::string("Empty URL");
   }
 
@@ -216,17 +226,23 @@ std::string restService(ConnectionInfo* ciP, RestService* serviceV)
   for (unsigned int ix = 0; serviceV[ix].treat != NULL; ++ix)
   {
     if ((serviceV[ix].components != 0) && (serviceV[ix].components != components))
+    {
       continue;
+    }
 
     if ((ciP->method != serviceV[ix].verb) && (serviceV[ix].verb != "*"))
+    {
       continue;
+    }
 
     strncpy(ciP->payloadWord, serviceV[ix].payloadWord.c_str(), sizeof(ciP->payloadWord));
     bool match = true;
     for (int compNo = 0; compNo < components; ++compNo)
     {
       if (serviceV[ix].compV[compNo] == "*")
+      {
         continue;
+      }
 
       if (strcasecmp(serviceV[ix].compV[compNo].c_str(), compV[compNo].c_str()) != 0)
       {
@@ -236,7 +252,9 @@ std::string restService(ConnectionInfo* ciP, RestService* serviceV)
     }
 
     if (match == false)
+    {
       continue;
+    }
 
     if ((ciP->payload != NULL) && (ciP->payloadSize != 0) && (serviceV[ix].verb != "*"))
     {
@@ -246,14 +264,19 @@ std::string restService(ConnectionInfo* ciP, RestService* serviceV)
       ciP->parseDataP = &parseData;
       response = payloadParse(ciP, &parseData, &serviceV[ix], &reqP, &jsonReqP);
       LM_T(LmtParsedPayload, ("payloadParse returns '%s'", response.c_str()));
+
       if (response != "OK")
       {
         restReply(ciP, response);
 
         if (reqP != NULL)
+        {
           reqP->release(&parseData);
+        }
         if (jsonReqP != NULL)
+        {
           jsonReqP->release(&parseData);
+        }
 
         compV.clear();
         return response;
@@ -283,9 +306,14 @@ std::string restService(ConnectionInfo* ciP, RestService* serviceV)
       restReply(ciP, response);
 
       if (reqP != NULL)
+      {
         reqP->release(&parseData);
+      }
+
       if (jsonReqP != NULL)
+      {
         jsonReqP->release(&parseData);
+      }
 
       compV.clear();
         
