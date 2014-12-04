23728353e078e29dc5c0afde1b05df04cba3e7c5
Ken Zangelin <kzangeli@tid.es>
2014-12-04 11:22:54 +0100

    removed 2 obsolete fixmes

--- src/lib/serviceRoutines/postUpdateContext.cpp
+++ src/lib/serviceRoutines/postUpdateContext.cpp
@@ -151,7 +151,6 @@ std::string postUpdateContext
     std::string     resource     = prefix + "/updateContext";
     std::string     tenant       = ciP->tenant;
 
-    // FIXME P5: Service-Path ("" after 'tenant' in call to sendHttpSocket
     out = sendHttpSocket(ip, port, protocol, verb, tenant, ciP->httpHeaders.servicePath, resource, "application/xml", payloadIn, false, true);
 
     // Should be safe to free up ucrP now ...
