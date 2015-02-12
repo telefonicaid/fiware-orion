b68311ecdb9455541e6cbe0774ac10bc66aae230
Ken Zangelin <kzangeli@tid.es>
2015-02-12 17:31:22 +0100

    change in functest 20 => 30

--- test/unittests/convenienceMap/mapDeleteIndividualContextEntityAttributes_test.cpp
+++ test/unittests/convenienceMap/mapDeleteIndividualContextEntityAttributes_test.cpp
@@ -28,7 +28,6 @@
 #include "logMsg/traceLevels.h"
 #include "common/globals.h"
 
-#include "convenienceMap/mapGetContextEntityAttributes.h"
 #include "convenience/RegisterProviderRequest.h"
 #include "convenienceMap/mapDeleteIndividualContextEntityAttributes.h"
 #include "mongoBackend/mongoRegisterContext.h"
