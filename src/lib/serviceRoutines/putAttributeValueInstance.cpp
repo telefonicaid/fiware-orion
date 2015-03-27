fa0a405a688fbb5d1111e0ef8f444baa1b56764b
Ken Zangelin <kzangeli@tid.es>
2015-03-27 21:42:45 +0100

    PUT /v1/contextEntities/{entity::id}/attributes/{attribute::name}/{metaID}

--- src/lib/serviceRoutines/putAttributeValueInstance.cpp
+++ src/lib/serviceRoutines/putAttributeValueInstance.cpp
@@ -27,11 +27,13 @@
 
 #include "logMsg/logMsg.h"
 
-#include "mongoBackend/mongoUpdateContext.h"
 #include "rest/ConnectionInfo.h"
-#include "rest/restReply.h"
 #include "ngsi/ParseData.h"
-#include "ngsi10/UpdateContextResponse.h"
+#include "ngsi/StatusCode.h"
+#include "rest/restReply.h"
+#include "rest/uriParamNames.h"
+#include "rest/EntityTypeInfo.h"
+#include "serviceRoutines/postUpdateContext.h"
 #include "serviceRoutines/putAttributeValueInstance.h"
 
 
@@ -40,9 +42,24 @@
 *
 * putAttributeValueInstance - 
 *
-* PUT /ngsi10/contextEntities/{entityID}/attributes/{attributeName}/{valueID}
+* PUT /v1/contextEntities/{entity::id}/attributes/{attribute::name}/{metaID}
+* PUT /ngsi10/contextEntities/{entity::id}/attributes/{attribute::name}/{metaID}
 *
-* Payload: UpdateContextAttributeRequest
+* Payload In:  UpdateContextAttributeRequest
+* Payload Out: StatusCode
+*
+* URI parameters
+*   - entity::type=TYPE
+*   - note that '!exist=entity::type' and 'exist=entity::type' are not supported by convenience operations
+*     that don't use the standard operation QueryContext as there is no Restriction otherwise.
+*     Here, entity::type can be empty though, and it is, unless the URI parameter 'entity::type=TYPE' is used.
+*
+* 01. Check validity of path components VS payload
+* 02. Fill in UpdateContextRequest
+* 03. Call postUpdateContext
+* 04. Fill in StatusCode from UpdateContextResponse
+* 05. Render result
+* 06. Cleanup and return result
 */
 std::string putAttributeValueInstance
 (
@@ -52,14 +69,55 @@ std::string putAttributeValueInstance
   ParseData*                 parseDataP
 )
 {
-  UpdateContextRequest            request;
-  UpdateContextResponse           response;
-  std::string                     entityId      = compV[2];
-  std::string                     attributeName = compV[4];
-  std::string                     valueId       = compV[5];
-  UpdateContextAttributeRequest*  upcarP        = &parseDataP->upcar.res;
-  bool                            idFound       = false;
+  std::string  entityId      = compV[2];
+  std::string  attributeName = compV[4];
+  std::string  metaID        = compV[5];
+  std::string  entityType    = ciP->uriParam[URI_PARAM_ENTITY_TYPE];
+  std::string  answer;
+  StatusCode   response;
 
+
+  // 01. Check validity of path components VS payload
+  Metadata* mP = parseDataP->upcar.res.metadataVector.lookupByName("ID");
+
+  if ((mP != NULL) && (mP->value != metaID))
+  {
+    std::string details = "unmatching metadata ID value URI/payload: /" + metaID + "/ vs /" + mP->value + "/";
+    
+    response.fill(SccBadRequest, details);
+    answer = response.render(ciP->outFormat, "", false, false);
+    parseDataP->upcar.res.release();
+
+    return answer;
+  }
+
+
+  // 02. Fill in UpdateContextRequest
+  parseDataP->upcr.res.fill(&parseDataP->upcar.res, entityId, entityType, attributeName, metaID, "UPDATE");
+
+
+  // 03. Call postUpdateContext
+  postUpdateContext(ciP, components, compV, parseDataP);
+
+
+  // 04. Fill in StatusCode from UpdateContextResponse
+  response.fill(parseDataP->upcrs.res);
+
+
+  // 05. Render result
+  answer = response.render(ciP->outFormat, "", false, false);
+
+
+  // 06. Cleanup and return result
+  response.release();
+  parseDataP->upcar.res.release();
+  parseDataP->upcr.res.release();
+  parseDataP->upcrs.res.release();
+
+  return answer;
+}
+
+#if 0
   //
   // Any metadata ID in the payload?
   //
@@ -146,3 +204,5 @@ std::string putAttributeValueInstance
   request.release();
   return statusCode.render(ciP->outFormat, "", false, false);
 }
+
+#endif
