e2794676a92e101e57a497bad7673056d425dfbb
brox <carlos.romerobrox@telefonica.com>
2015-05-29 09:00:40 +0200

    Do not use mutex for empty contexts

--- src/lib/rest/curl_context.cpp
+++ src/lib/rest/curl_context.cpp
@@ -2,6 +2,8 @@
 
 #include <map>
 
+#include <assert.h>
+
 #include "logMsg/logMsg.h"
 #include "logMsg/traceLevels.h"
 
@@ -12,7 +14,7 @@ static std::map<std::string, struct curl_context> contexts;
 struct curl_context
 get_curl_context(const std::string& key)
 {
-  struct curl_context cc;
+  struct curl_context cc = {NULL, NULL};
   int s = pthread_mutex_lock(&contexts_mutex);
   if(s!=0)
   {
@@ -24,23 +26,27 @@ get_curl_context(const std::string& key)
   {
       //not found, create it
       cc.curl = curl_easy_init();
-      pthread_mutex_t *pm = (pthread_mutex_t *) malloc(sizeof(*pm));
-      if (pm==NULL)
-      {
-          LM_X(1,("malloc"));
-      }
-      int s = pthread_mutex_init(pm, NULL);
-      if(s!=0)
-      {
-           LM_X(1,("pthread_mutex_init"));
-      }
-      cc.pmutex = pm;
       if (cc.curl != NULL)
       {
+        pthread_mutex_t *pm = (pthread_mutex_t *) malloc(sizeof(*pm));
+        if (pm==NULL)
+        {
+          LM_X(1,("malloc"));
+        }
+        int s = pthread_mutex_init(pm, NULL);
+        if(s!=0)
+        {
+           LM_X(1,("pthread_mutex_init"));
+        }
+        cc.pmutex = pm;
         contexts[key] = cc;
       }
+      else  // curl_easy returned null
+      {
+       cc.pmutex = NULL;    //unnecessary but clearer
+      }
   }
-  else
+  else //previuos context found
   {
       cc = it->second;
   }
@@ -49,11 +55,19 @@ get_curl_context(const std::string& key)
   {
     LM_X(1,("pthread_mutex_unlock"));
   }
-  s = pthread_mutex_lock(cc.pmutex);
-  if(s!=0)
-  {
+
+  // lock the mutex, if everything was right
+  // and cc is not {NULL, NULl}
+  if (cc.pmutex != NULL) {
+    s = pthread_mutex_lock(cc.pmutex);
+    if(s!=0)
+    {
       LM_X(1,("pthread_mutex_lock"));
+    }
   }
+
+  assert((cc.curl != NULL && cc.pmutex != NULL)
+          || (cc.curl == NULL && cc.pmutex == NULL));
   return cc;
 }
 
@@ -61,11 +75,18 @@ get_curl_context(const std::string& key)
 void
 release_curl_context(struct curl_context cc)
 {
-  int s = pthread_mutex_unlock(cc.pmutex);
-  if(s!=0)
-  {
+  assert((cc.curl != NULL && cc.pmutex != NULL)
+          || (cc.curl == NULL && cc.pmutex == NULL));
+
+  // Unlock the mutex if not an empty context
+  if (cc.pmutex != NULL) {
+    int s = pthread_mutex_unlock(cc.pmutex);
+    if(s!=0)
+    {
       LM_X(1,("pthread_mutex_unlock"));
+    }
   }
+  // Reset context if not an empty context
   if (cc.curl != NULL)
   {
     curl_easy_reset(cc.curl);
