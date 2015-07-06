0a88a6a1c0edf82d7d8c2cb76ba2e9a0ac8d3602
Jose Manuel Cantera <jmcanterafonseca@gmail.com>
2015-07-06 15:32:52 +0200

    Docker Ubuntu Second iteration

--- docker/dev/ubuntu/scripts/examples/orion-dev.sh
+++ docker/dev/ubuntu/scripts/examples/orion-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 source $HOME/.bashrc
-docker --tlsverify=false run -p 1026:1026 --name orionDev --ipv6 --link mongoOrion:mongoOrion -v /Users/jmcf/work/develop/fiware-orion:/root/src -t -i orion-dev:latest /bin/bash
+docker --tlsverify=false run -p 1026:1026 --name orionDev --link mongoOrion:mongoOrion -v /Users/jmcf/work/develop/fiware-orion:/root/src -t -i orion-dev:latest /bin/bash
