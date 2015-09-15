5436907941bc1264dfa81b02f4c3aed51f6440c9
Fermin Galan Marquez <fermin@tid.es>
2015-09-15 15:44:38 +0200

    FIX FIWARE Lab image building scripts

---
+++ scripts/build/fiwarelab/README.md
@@ -0,0 +1,3 @@
+This directory provides the scripts for building and checking Orion images at FIWARE Lab. Please have a look to the
+[FIWARE Lab Image Deployment Guidelines](https://forge.fiware.org/plugins/mediawiki/wiki/testbed/index.php/FIWARE_LAB_Image_Deployement_Guideline)
+for additional information.
---
+++ scripts/build/fiwarelab/check-fiwarelab-image.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Copyright 2013 Telefonica Investigacion y Desarrollo, S.A.U
+#
+# This file is part of Orion Context Broker.
+#
+# Orion Context Broker is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Orion Context Broker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Orion Context Broker. If not, see http://www.gnu.org/licenses/.
+#
+# For those usages not covered by this license please contact with
+# iot_support at tid dot es
+
+# We assume the following preconditions in the place where this script runs: 
+#
+# - The IP environement variable holds the IP of the VM to be checked
+# - Connectivity to port 1026 in the VM to be checked is working
+# - The following commands are installed and accesible in the default PATH: curl
+
+curl $IP:1026/version
