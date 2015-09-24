910a02984dd7fc69ca6e7173fca044247a2cf324
brox <carlos.romerobrox@telefonica.com>
2015-09-24 11:41:32 +0200

    Fix style

--- src/lib/common/JsonHelper.cpp
+++ src/lib/common/JsonHelper.cpp
@@ -29,19 +29,21 @@ std::string toJsonString(const std::string& input)
 {
   std::ostringstream ss;
   ss << '"';
-  for (std::string::const_iterator iter = input.begin(); iter != input.end(); ++iter) {
-        switch (*iter) {
-            case '\\': ss << "\\\\"; break;
-            case '"': ss << "\\\""; break;
-            case '/': ss << "\\/"; break;
-            case '\b': ss << "\\b"; break;
-            case '\f': ss << "\\f"; break;
-            case '\n': ss << "\\n"; break;
-            case '\r': ss << "\\r"; break;
-            case '\t': ss << "\\t"; break;
-            default: ss << *iter; break;
-      }
-  }
+  for (std::string::const_iterator iter = input.begin(); iter != input.end(); ++iter)
+    {
+      switch (*iter)
+        {
+        case '\\': ss << "\\\\"; break;
+        case '"': ss << "\\\""; break;
+        case '/': ss << "\\/"; break;
+        case '\b': ss << "\\b"; break;
+        case '\f': ss << "\\f"; break;
+        case '\n': ss << "\\n"; break;
+        case '\r': ss << "\\r"; break;
+        case '\t': ss << "\\t"; break;
+        default: ss << *iter; break;
+        }
+    }
   ss << '"';
   return ss.str();
 }
