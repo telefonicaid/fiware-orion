34e1f0b97f0c5266734c0424e63e628b9e9fde32
Fermin Galan Marquez <fermin.galanmarquez@telefonica.com>
2015-12-10 15:31:53 +0100

    ADD unit test for apiTypesV2 classes

---
+++ test/unittests/apiTypesV2/EntityVector_test.cpp
@@ -0,0 +1,86 @@
+/*
+*
+* Copyright 2015 Telefonica Investigacion y Desarrollo, S.A.U
+*
+* This file is part of Orion Context Broker.
+*
+* Orion Context Broker is free software: you can redistribute it and/or
+* modify it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* Orion Context Broker is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+* General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Orion Context Broker. If not, see http://www.gnu.org/licenses/.
+*
+* For those usages not covered by this license please contact with
+* iot_support at tid dot es
+*
+* Author: Fermin Galan
+*/
+
+#include "apiTypesV2/EntityVector.h"
+#include "unittest.h"
+
+/* ****************************************************************************
+*
+* present - no output expected, just exercising the code
+*/
+TEST(EntityVector, present)
+{
+  utInit();
+
+  Entity* enP    = new Entity();
+  enP->id        = "E";
+  enP->type      = "T";
+  enP->isPattern = "false";
+
+  ContextAttribute* caP = new ContextAttribute("A", "T", "val");
+  enP->attributeVector.push_back(caP);
+
+  EntityVector eV;
+  eV.push_back(enP);
+
+  eV.present("");
+
+  utExit();
+}
+
+
+/* ****************************************************************************
+*
+* check
+*/
+TEST(EntityVector, check)
+{
+  ConnectionInfo ci;
+
+  utInit();
+
+  Entity* enP;
+
+  // EntityVector with ok Entity inside
+  enP            = new Entity();
+  enP->id        = "E";
+  enP->type      = "T";
+  enP->isPattern = "false";
+  EntityVector enV1;
+  enV1.push_back(enP);
+
+  // EntityVector with nok Entity inside
+  enP            = new Entity();
+  enP->id        = "";
+  enP->type      = "T";
+  enP->isPattern = "false";
+  EntityVector enV2;
+  enV2.push_back(enP);
+
+  EXPECT_EQ("OK", enV1.check(&ci, EntitiesRequest));
+  EXPECT_EQ("No Entity ID", enV2.check(&ci, EntitiesRequest));
+
+  utExit();
+}
