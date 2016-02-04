85e3825080ba54d6a0e0911acc51e2740f6dc196
brox <carlos.romerobrox@telefonica.com>
2016-02-04 13:28:31 +0100

    Add missing header file for error messages

---
+++ src/lib/common/errorMessages.h
@@ -0,0 +1,7 @@
+#ifndef SRC_LIB_COMMON_ERRORMESSAGES_H
+#define SRC_LIB_COMMON_ERRORMESSAGES_H
+
+#define MORE_MATCHING_ENT "More than one matching entity. Please refine your query"
+
+
+#endif // SRC_LIB_COMMON_ERRORMESSAGES_H
