cda3415d3db12794c6d750e117641020b54e9881
Ken Zangelin <kzangeli@tid.es>
2016-03-21 12:18:16 +0100

    Fixed an uninitialized variable

--- src/lib/ngsi/Metadata.cpp
+++ src/lib/ngsi/Metadata.cpp
@@ -153,6 +153,12 @@ Metadata::Metadata(const BSONObj& mdB)
 {
   name = getStringFieldF(mdB, ENT_ATTRS_MD_NAME);
   type = mdB.hasField(ENT_ATTRS_MD_TYPE) ? getStringFieldF(mdB, ENT_ATTRS_MD_TYPE) : "";
+
+  if (type != "")
+  {
+    typeGiven = true;
+  }
+
   switch (getFieldF(mdB, ENT_ATTRS_MD_VALUE).type())
   {
   case String:
