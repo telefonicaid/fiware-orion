33376ad6ccb65cc6c4f965f9193aff1b47a3b914
Fermin Galan Marquez <fermin.galanmarquez@telefonica.com>
2016-04-27 10:57:15 +0200

    FIX urls that make Orion to crash

--- src/lib/common/string.cpp
+++ src/lib/common/string.cpp
@@ -254,14 +254,20 @@ bool parseUrl(const std::string& url, std::string& host, int& port, std::string&
 
   /* First: split by the first '/' to get host:ip and path */
   std::vector<std::string>  urlTokens;
-  int                       components = stringSplit(url, '/', urlTokens);
-
-  protocol = urlTokens[0];
+  int                       components = stringSplit(url, '/', urlTokens);  
 
   //
   // Ensuring the scheme is present
   //
-  if ((protocol != "https:") && (protocol != "http:"))
+  if ((urlTokens.size() == 0) || ((urlTokens[0] != "https:") && (urlTokens[0] != "http:")))
+  {
+    return false;
+  }
+
+  //
+  // Ensuring the host is present
+  //
+  if (urlTokens[2] == "")
   {
     return false;
   }
