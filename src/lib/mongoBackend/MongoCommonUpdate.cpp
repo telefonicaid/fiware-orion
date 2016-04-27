8833f26c83a82ae273165d8dfc04f2a507428678
Ken Zangelin <kzangeli@tid.es>
2016-04-27 12:30:21 +0200

    Parenthesis in 'if'

--- src/lib/mongoBackend/MongoCommonUpdate.cpp
+++ src/lib/mongoBackend/MongoCommonUpdate.cpp
@@ -1476,7 +1476,7 @@ static bool processSubscriptions
     }
 
     /* Check 2: String Filter */
-    if (trigs->stringFilterP && !trigs->stringFilterP->match(notifyCerP))
+    if ((trigs->stringFilterP) && (!trigs->stringFilterP->match(notifyCerP)))
     {
       continue;
     }
