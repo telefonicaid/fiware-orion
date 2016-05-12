ad8760a3b1900d2e3342471c84735717c3d9b669
brox <carlos.romerobrox@telefonica.com>
2016-05-12 14:03:01 +0200

    Add 'empty' mongoUpdateSubscription for patchSubscription

---
+++ src/lib/mongoBackend/mongoUpdateSubscription.h
@@ -0,0 +1,58 @@
+#ifndef SRC_LIB_MONGOBACKEND_MONGOUPDATESUBSCRIPION_H
+#define SRC_LIB_MONGOBACKEND_MONGOUPDATESUBSCRIPION_H
+
+/*
+*
+* Copyright 2016 Telefonica Investigacion y Desarrollo, S.A.U
+*
+* This file is part of Orion Context Broker.
+*
+* Orion Context Broker is free software: you can redistribute it and/or
+* modify it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* Orion Context Broker is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+* General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Orion Context Broker. If not, see http://www.gnu.org/licenses/.
+*
+* For those usages not covered by this license please contact with
+* iot_support at tid dot es
+*
+* Author: Fermin Galan
+*/
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include "rest/OrionError.h"
+#include "apiTypesV2/SubscriptionUpdate.h"
+
+
+
+/* ****************************************************************************
+*
+* mongoUpdateSubscription -
+*
+* Returns:
+* - subId: subscription susscessfully updated ('oe' must be ignored), the subId
+*   must be used to fill Location header
+* - "": subscription creation fail (look to 'oe')
+*/
+extern std::string mongoUpdateSubscription
+(
+  const ngsiv2::SubscriptionUpdate&    sub,
+  OrionError*                          oe,
+  std::map<std::string, std::string>&  uriParams,
+  const std::string&                   tenant,
+  const std::vector<std::string>&      servicePathV,
+  const std::string&                   xauthToken,
+  const std::string&                   fiwareCorrelator
+);
+
+#endif // SRC_LIB_MONGOBACKEND_MONGOUPDATESUBSCRIPION_H
