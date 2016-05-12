b64068bb7dea958c99ec68437a000f10bf60b973
Ken Zangelin <kzangeli@tid.es>
2016-05-12 17:11:37 +0200

    removed an unwanted comment

--- src/lib/mongoBackend/mongoSubscribeContext.cpp
+++ src/lib/mongoBackend/mongoSubscribeContext.cpp
@@ -223,7 +223,6 @@ HttpStatusCode mongoSubscribeContext
 
     LM_T(LmtSubCache, ("inserting a new sub in cache (%s)", oidString.c_str()));
 
-    // FIXME Px: In the near future, the subscription carries the HttpInfo
     ngsiv2::HttpInfo httpInfo;
 
     httpInfo.url = requestP->reference.get();
