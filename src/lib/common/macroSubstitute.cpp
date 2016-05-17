f06402ced4617444b0441480909272f75e7053b1
Ken Zangelin <kzangeli@tid.es>
2016-05-17 14:28:27 +0200

    Lots of comments in the code for macro substitutions

--- src/lib/common/macroSubstitute.cpp
+++ src/lib/common/macroSubstitute.cpp
@@ -61,7 +61,7 @@ static void attributeValue(std::string* valueP, const std::vector<ContextAttribu
     {
       *valueP = "null";
     }
-    else if (vec[ix]->valueType == orion::ValueTypeObject)  // meaning: Compound Value
+    else if ((vec[ix]->valueType == orion::ValueTypeObject) || (vec[ix]->valueType == orion::ValueTypeVector))
     {
       if (vec[ix]->compoundValueP)
       {
@@ -75,17 +75,15 @@ static void attributeValue(std::string* valueP, const std::vector<ContextAttribu
         }
         else
         {
+          LM_E(("Runtime Error (attribute is of object type but its compound is of invalid type)"));
           *valueP = "";
         }
       }
       else
       {
+        LM_E(("Runtime Error (attribute is of object type but has no compound)"));
         *valueP = "";
       }
-    }
-    else if (vec[ix]->valueType == orion::ValueTypeObject)
-    {
-      *valueP = "{" + vec[ix]->compoundValueP->toJson(true) + "}";
     }
     else
     {
@@ -104,6 +102,22 @@ static void attributeValue(std::string* valueP, const std::vector<ContextAttribu
 /* ****************************************************************************
 *
 * macroSubstitute - 
+*
+* An initial out-buffer of CHUNK_SIZE is allocated, and later on during the substitution if the
+* out-buffer is not big enough, it is realloced with another CHUNK_SIZE.
+*
+* 1024 is set as CHUNK_SIZE and hopefully for most substitutions it will be enough with ONE allocation.
+* Allocation is very costly operations ...
+* This function could be a lot faster is we took the first 1024 bytes from the stack instead of using calloc.
+* However, the function gets a little more complicated like that as the first reallos would have to be a normal malloc and a memcpy.
+*
+* Variable used:
+*   - toP:     pointer to the beginning of the 'to string' where to add the new characters from the 'from string'
+*   - toIx:    the index of the 'to string', always at the end of the string, where to append characters.
+*              toIx also serves as the running size of the 'to string'
+*   - toLen:   Total size of the 'to string'
+*   - fromP:   pointer to the currently analysed character in the 'from string'
+*
 */
 #define CHUNK_SIZE 1024
 void macroSubstitute(std::string* to, const std::string& from, const ContextElement& ce)
@@ -122,50 +136,90 @@ void macroSubstitute(std::string* to, const std::string& from, const ContextElem
 
   while (*fromP != 0)
   {
-    if ((*fromP == '$') && (fromP[1] == '{'))
+    //
+    // If the current position in the 'from string' (fromP), points to the two characters
+    // "${", then (if an ending '}' is found) a variable substitution is to be performed.
+    //
+    // Otherwise, just copy the character in 'from string' to 'to string'.
+    //
+    if ((fromP[0] == '$') && (fromP[1] == '{'))
     {
+      //
+      // varP points to the character after "${", i.e. the first character of the variable name.
+      // E.g. if 'from string' is "${abc}", varP would point to the 'a'
+      //
       char* varP = (char*) &fromP[2];
 
+      // Set fromP to point to the chatactrer after "${" - the first character of the variable name
       fromP += 2;
-      while ((*fromP != 0) && (*fromP != '}'))
+
+      // Find the closing '}'
+      while ((fromP[0] != 0) && (*fromP != '}'))
       {
         ++fromP;
       }
 
-      if (*fromP == 0)
+      // If the 'from string' ends without finding a closing '}', ERROR
+      if (fromP[0] == 0)
       {
         break;  // ERROR: non-terminated variable
       }
 
+      // Terminate the variable name, by replacing the '}' with a ZERO
       *((char*) fromP) = 0;
+
+      // Then make fromP point to the character AFTER the closing '}'
       ++fromP;
 
       // Variable found, get its value
-      char* substitute    = NULL;
-      int   substituteLen = 0;
+      char*        substitute    = NULL;
+      int          substituteLen = 0;
+      std::string  value;
 
       if (strcmp(varP, "id") == 0)
       {
+        //
+        // ${id} => substitute with ID of the entity
+        //
         substitute = (char*) ce.entityId.id.c_str();
       }
       else if (strcmp(varP, "type") == 0)
       {
+        //
+        // ${type} => substitute with TYPE of the entity
+        //
         substitute = (char*) ce.entityId.type.c_str();
       }
       else  // attribute
       {
-        std::string value;
-
+        //
+        // Neither ${id} nor ${type} => must be a name of an attribute ( ${ATTR_NAME} )
+        // The value of the attribute ATTR_NAME is extracted by the function attributeValue
+        // The variable 'substitute' is set to point to it
+        //
         attributeValue(&value, ce.contextAttributeVector.vec, varP);
         substitute = (char*) value.c_str();
       }
 
+      //
+      // If substitution found, get its string length
+      // If not found, substituteLen is ZERO already
+      //
       if (substitute != NULL)
       {
         substituteLen = strlen(substitute);
       }
 
+      //
       // Room enough?  If not, realloc
+      //   toIx            is the current length of the 'to string'.
+      //   substituteLen   is the length of the value to put in instead of ${XXX}
+      //   toLen           is the TOTAL length of the 'to string'
+      //
+      // So, if toIx + substituteLen is bigger than THE TOTAL LENGTH of the 'to string'
+      // we will have to get more memory before we insert the 'substitute' in the 'to string'
+      // [ The '- 1' is for the zero-termination of the 'in string'.
+      //
       if (toIx + substituteLen >= toLen - 1)
       {
         toP    = (char*) realloc(toP, toLen + CHUNK_SIZE);
@@ -179,6 +233,13 @@ void macroSubstitute(std::string* to, const std::string& from, const ContextElem
         }
       }
 
+      //
+      // Now, if a substitute was found, append it to 'to string' and add the length of the
+      // substitute to 'toIx', so that toP[toIx] point to the last non-used char of the 'to string'
+      //
+      // If no substitution was found (attribute with that name does not exist), then the ${XXX}
+      // is substituted with NOTHING - it is silently consumed.
+      //
       if (substitute != NULL)
       {
         // Concatenate 'substitute' to toP
@@ -191,11 +252,35 @@ void macroSubstitute(std::string* to, const std::string& from, const ContextElem
     }
     else
     {
-      toP[toIx++] = *fromP;
+      //
+      // Room enough?  If not, realloc
+      // See detaild description of the realloc step above
+      //
+      if (toIx >= toLen - 1)
+      {
+        toP    = (char*) realloc(toP, toLen + CHUNK_SIZE);
+        toLen += CHUNK_SIZE;
+
+        if (toP == NULL)
+        {
+          LM_E(("Runtime Error (out of memory)"));
+          *to = "";
+          return;
+        }
+      }
+
+      //
+      // Simply copy the char from 'from string' into 'to string'
+      // and step over that char to prepare for the next one.
+      //
+      toP[toIx++] = fromP[0];
       ++fromP;
     }
   }
 
+  //
+  // Set the incoming std::string to the value of 'to string' and free the 'to string'
+  //
   *to = toP;
   free(toP);
 }
