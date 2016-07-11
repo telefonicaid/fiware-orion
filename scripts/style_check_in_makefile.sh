b7174c27d5173baed313e6a29b3ebc7cf3770a3c
Ken Zangelin <kzangeli@tid.es>
2016-07-11 11:01:48 +0200

    Removed the two new libs from the list of remaining libs

--- scripts/style_check_in_makefile.sh
+++ scripts/style_check_in_makefile.sh
@@ -52,9 +52,7 @@ style_check src/lib/alarmMgr
 
 # FIXME: Just keep adding directories here until all of them are included:
 #
-# style_check src/lib/alarmMgr
 # style_check src/lib/apiTypesV2
-# style_check src/lib/cache
 # style_check src/lib/common
 # style_check src/lib/convenience
 # style_check src/lib/jsonParse
