c595808b1add8a7b2e07ba641013a758a9b938f0
IVAN ARIAS LEON <ivan.ariasleon@telefonica.com>
2016-12-09 13:18:24 +0100

    [FIX] rename and typos

--- test/loadTest/connections_stress_tests/connections_stress_tests.py
+++ test/loadTest/connections_stress_tests/connections_stress_tests.py
@@ -30,15 +30,15 @@ import datetime
 import pymongo
 
 
-class Stablish_Connections:
+class Stablished_Connections:
     """
-    Verify that ContextBroker works properly with a large number of stablish connections
+    Verify that ContextBroker works properly with a large number of stablished connections
     Tests procedure:
     - drop the db in mongo
-    - create 5000 subscription with subject.entities.idPattern:. *
+    - create 5000 subscription with subject.entities.idPattern: .*
     - verify/modify the notification listener with a delay of 10 minutes before answering.
     - modify the ContextBroker config with: -httpTimeout 600000 and restart it.
-    - modify a entity each 3 minutes, that it triggers all subscriptions.
+    - launch an entity update, that it triggers all subscriptions.
     - launch indefinitely a "/version" request per second and verify that its response is correct.
     """
     # variables
@@ -52,7 +52,6 @@ class Stablish_Connections:
     mongo_port = u'27017'
     verbose = False
     duration = 60       # time in minutes
-    update_delay = 180  # time in seconds
     version_delay = 1   # time in seconds
 
     @staticmethod
@@ -61,7 +60,7 @@ class Stablish_Connections:
         usage message
         """
         print " *****************************************************************************************************************"
-        print " * This script Verify that CB works properly with a large number of stablish connections:                        *"
+        print " * This script verifies that CB works properly with a large number of stablished connections:                    *"
         print " *                                                                                                               *"
         print " *  Parameters:                                                                                                  *"
         print " *     -host=<host>         : CB host (OPTIONAL) (default: localhost).                                           *"
@@ -74,11 +73,11 @@ class Stablish_Connections:
         print " *     -duration=<value>    : test duration, value is in minutes (OPTIONAL) (default: 60 minutes)                *"
         print " *                                                                                                               *"
         print " *  Examples:                                                                                                    *"
-        print " *    python stablish_connections.py -host=10.10.10.10 -notif_url=http://10.0.0.1:1234/notify duration=100 -v    *"
+        print " *   python connections_stress_tests.py -host=10.10.10.10 -notif_url=http://10.0.0.1:1234/notify duration=100 -v *"
         print " *                                                                                                               *"
         print " *  Note:                                                                                                        *"
-        print " *    - the update delay is 3 minutes                                                                            *"
         print " *    - the version delay is a second                                                                            *"
+        print " *    - the number of subscriptions is 5000                                                                      *"
         print " *                                                                                                               *"
         print " *****************************************************************************************************************"
         exit(0)
@@ -106,6 +105,7 @@ class Stablish_Connections:
         """
         Contructor
         """
+        print("Test init: %s " % self.__convert_timestamp_to_zulu(time.time()))
         for i in range(len(arguments)):
             if arguments[i].find('-u') >= 0:
                 self.__usage()
@@ -125,6 +125,7 @@ class Stablish_Connections:
                 self.duration = float(arguments[i].split("=")[1])
 
         self.headers = {u'Accept': u'application/json',
+                        u'Content-Type': u'application/json',
                         u'Fiware-Service': self.service,
                         u'Fiware-ServicePath': self.service_path}
 
@@ -135,12 +136,12 @@ class Stablish_Connections:
         print(" INFO  - notification URL: %s" % self.notif_url)
         print(" INFO  - mongo host: %s" % self.mongo_host)
         print(" INFO  - test duration: %s minutes (%s seconds)" % (str(self.duration), str(self.duration * 60)))
-        print(" INFO  - updates requests delay: %d seconds" % self.update_delay)
         print(" INFO  - version requests delay: %d seconds" % self.version_delay)
         print(" INFO  - max subcription: %d" % self.max_subscription_created)
         print(" ***************************************************************************************")
         print(" * WARN  - verify if the listener has a delay in the response (10 minutes recommended) *")
-        print(" * WARN  - verify if -httpTimeout 600000 parameter is used in CB config                *")
+        print(" * WARN  - verify if these parameters are used in CB config:                           *")
+        print("            -httpTimeout 600000 -notificationMode threadpool:60000:5000                *")
         print(" ***************************************************************************************")
 
     def drop_database(self):
@@ -161,7 +162,6 @@ class Stablish_Connections:
         create N subscription until de max subscription created
         """
         payload = u'{"description": "subscription used to epoll test",  "subject": {"entities": [{"idPattern": ".*"}], "condition": {"attrs": ["temperature"]}}, "notification": {"http": {"url": "%s"}, "attrs": ["temperature"]}}' % self.notif_url
-        self.headers["Content-Type"] = u'application/json'
         for i in range(self.max_subscription_created):
             resp = requests.post("%s/v2/subscriptions" % self.cb_endpoint, headers=self.headers, data=payload)
             self.__print_by_console(resp)
@@ -170,24 +170,20 @@ class Stablish_Connections:
 
     def update_and_version(self):
         """
-        modify a entity each 3 minutes, that it triggers all subscriptions and
+        launch an entity update, that it triggers all subscriptions and
         launch indefinitely a "/version" request and verify that its response is correct
         """
         init_date = time.time()
         duration_in_secs = self.duration * 60
-        print("Test init: %s " % self.__convert_timestamp_to_zulu(init_date))
-        update_time = time.time()
+
+        # update request
+        random_value = random.randint(1, 100)  # random value between 1..100 for attribute value. See the payload below.
+        payload = u'{"actionType": "APPEND", "entities": [{"id": "Bcn-Welt", "temperature": {"value": %s}}]}' % random_value
+        resp = requests.post("%s/v2/op/update" % self.cb_endpoint, headers=self.headers, data=payload)
+        assert resp.status_code == 204, " ERROR - the update batch op request is failed: \n - status code: %s \n - response: %s" % (str(resp.status_code), resp.text)
+        self.__print_by_console(resp)
+
         while (init_date+duration_in_secs) > time.time():
-            if update_time+self.update_delay < time.time():
-                # update request
-                self.headers["Content-Type"] = u'application/json'
-                payload = u'{"actionType": "APPEND", "entities": [{"id": "Bcn-Welt", "temperature": {"value": %s}}]}'
-                random_value = random.randint(1, 100)  # random value between 1..100 for attribute value. See the payload above.
-                print("payload: %s" % (payload % random_value))
-                resp = requests.post("%s/v2/op/update" % self.cb_endpoint, headers=self.headers, data=payload % random_value)
-                assert resp.status_code == 204, " ERROR - the update batch op request is failed: \n - status code: %s \n - response: %s" % (str(resp.status_code), resp.text)
-                self.__print_by_console(resp)
-                update_time = time.time()
             # version request
             resp = requests.get("%s/version" % self.cb_endpoint)
             self.__print_by_console(resp)
@@ -198,7 +194,7 @@ class Stablish_Connections:
 
 
 if __name__ == '__main__':
-    conn = Stablish_Connections(sys.argv)
+    conn = Stablished_Connections(sys.argv)
 
     # drop database in mongo
     conn.drop_database()
