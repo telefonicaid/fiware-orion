6f04a860e99de789cda3781a4afd6b4f6ceeed3c
Ken Zangelin <kzangeli@gmail.com>
2018-01-22 21:02:28 +0100

    Added a FIXME

--- src/lib/serviceRoutinesV2/postRegistration.cpp
+++ src/lib/serviceRoutinesV2/postRegistration.cpp
@@ -58,9 +58,10 @@ std::string postRegistration
   ParseData*                 parseDataP
 )
 {
+  // FIXME P4: See issue #3078 about servicePath for registrations
+  std::string           servicePath = (ciP->servicePathV[0] == "")? SERVICE_PATH_ROOT : ciP->servicePathV[0];
   ngsiv2::Registration  registration;
   OrionError            oe;
-  std::string           servicePath = (ciP->servicePathV[0] == "")? SERVICE_PATH_ROOT : ciP->servicePathV[0];
   std::string           regId;
   std::string           answer;
 
