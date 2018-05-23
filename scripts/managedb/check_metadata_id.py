d57e8d407847b0e3955999c632dea70aced1ceb2
Fermin Galan Marquez <fermin.galanmarquez@telefonica.com>
2018-05-23 21:31:18 +0200

    REFACTOR check_metadata_id.py re-written

--- scripts/managedb/check_metadata_id.py
+++ scripts/managedb/check_metadata_id.py
@@ -26,6 +26,7 @@ __author__ = 'fermin'
 from pymongo import MongoClient
 import json
 import sys
+from datetime import datetime
 from time import sleep
 
 ATTRS = 'attrs'
@@ -76,10 +77,125 @@ def update_ok(doc, check_attrs):
     return True
 
 
+def extract_attr_with_md_id(attrs):
+    """
+    Given a key-value of attributes, returns a dictionary with all attributes with metadata ID.
+    The key in the dictionary is the attribute name and the value a list of its IDs. If attrs
+    doesn't have any metadata ID, then an empty dictionary will be returned
+
+    :param attrs: key-value of attribute to process
+    :return: a dictionary as described above
+    """
+
+    r = {}
+    for attr in attrs.keys():
+        if attr.find('()') > 0:
+            tokens = attr.split("()")
+            name = tokens[0]
+            md_id = tokens[1]
+            if name in r.keys():
+                r[name].append(md_id)
+            else:
+                r[name] = [md_id]
+    return r
+
+
+def date2string(time):
+    """
+    Convert date to string
+
+    :param time: date time (as timestamp)
+    :return: string representing the date time
+    """
+
+    return datetime.fromtimestamp(time).strftime("%Y-%m-%dT%H:%M:%SZ")
+
+
+def fix_entity(entity, attrs_with_md_id):
+    """
+    Attemp to fix an entity.
+    :params entity: entity document
+    :param attrs_with_md_id: the attrs used to fix
+    :return: 'untouched', 'changed', 'error', depending of the result
+    """
+
+    new_attrs = dict(entity[ATTRS])
+    new_attrnames = list(entity[ATTRNAMES])
+    check_attrs = []
+    entity_touched = False
+    if autofix == 'as_new_attrs':
+        for base_name in attrs_with_md_id.keys():
+            for md_id in attrs_with_md_id[base_name]:
+                # Composing old name and new name, eg. temperature()ID1 and temperature:ID1
+                old_name = base_name + '()' + md_id
+                new_name = base_name + ':' + md_id
+
+                # Remove old base-name from attrNames list (e.g. temperature)...
+                if base_name in new_attrnames:
+                    new_attrnames.remove(base_name)
+
+                # ...and add the new one (temperature:ID1). Previous check of temperature:ID1 is not already
+                # in the list: that will be an unsolvable situation
+                if new_name in new_attrnames:
+                    print "     * ERROR: attribute <{}> already exist in entity. Cannot be automatically fixed".format(new_name)
+                    return 'error'
+                else:
+                    new_attrnames.append(new_name)
+
+                    # Changing temperature()ID1 for temperature:ID1 in the attrs key-map
+                    new_attrs[new_name] = new_attrs.pop(old_name)
+
+                    check_attrs.append(new_name)
+
+                    entity_touched = True
+
+    elif autofix == 'as_metadata':
+        # This is possible only if there isn't any attribute with more than one ID
+        for base_name in attrs_with_md_id.keys():
+            if len(attrs_with_md_id[base_name]) > 1:
+                print '     * ERROR: entity has at least one attribute with more than one ID. Cannot be automaticaly fixed.'
+                return 'error'
+
+        # Composing old name and new name, eg. temperature()ID1 and temperature:ID1
+        md_id = attrs_with_md_id[base_name][0]
+        old_name = base_name + '()' + md_id
+
+        # Changing temperature()ID1 for temperature in the attrs key-map...
+        new_attrs[base_name] = new_attrs.pop(old_name)
+
+        # ...and add the corresponding metadata ID (as regular metatada)
+        new_md_id = {
+            'type': 'string',   # This is the hardwired type used for metadata ID in Orion <= 1.13.0
+            'value': md_id
+        }
+        if 'md' in new_attrs[base_name].keys():
+            new_attrs[base_name]['md']['id'] = new_md_id
+        else:
+            new_attrs[base_name]['md'] = {'id': new_md_id}
+
+        entity_touched = True
+
+    # Now that new_attrs and new_attrsname are prepared, lest's do the actual update at MongoDB
+    if entity_touched:
+        # Update document with the new attribute fields
+        db[COL].update(flatten(doc['_id']), {'$set': {ATTRS: new_attrs, ATTRNAMES: new_attrnames}})
+
+        # Check update was ok (this is not an exhaustive checking that is better than nothing :)
+        check_doc = db[COL].find_one(flatten(doc['_id']))
+
+        if update_ok(check_doc, check_attrs):
+            return 'changed'
+        else:
+            print '     * ERROR: document <{}> change attempt failed!'.format(json.dumps(check_doc['_id']))
+            return 'error'
+    else:
+        return 'untouched'
+
+
 ##########################
 # Main program starts here
 
-autofix = False
+autofix = None
 
 if len(sys.argv) != 2:
     print "invalid number of arguments, please check https://fiware-orion.readthedocs.io/en/master/admin/upgrading_crossing_1-14-0/index.html"
@@ -89,7 +205,7 @@ DB = sys.argv[1]
 COL = 'entities'
 
 # Warn user
-if autofix:
+if autofix is not None:
     print "WARNING!!!! This script modifies your '%s' database. It is STRONGLY RECOMMENDED that you" % DB
     print "do a backup of your database before using it as described in https://fiware-orion.readthedocs.io/en/master/admin/database_admin/index.html#backup. Use this script at your own risk."
     print "If you are sure you want to continue type 'yes' and press Enter"
@@ -99,15 +215,25 @@ if autofix:
     if (confirm != 'yes'):
         sys.exit()
 
-client = MongoClient('localhost', 27017)
+uri = 'mongodb://localhost:27017'
+client = MongoClient(uri)
 db = client[DB]
 
 need_fix = False
+processed = 0
 
-untouched   = 0
-changed     = 0
-error       = 0
-processed   = 0
+# At the end, processed = no id + id duplicate attrs + id single attrs
+counter_analysis = {
+    'no': 0,
+    'dup': 0,
+    'single': 0,
+}
+# At the end, processed = untouched + changed + error
+counter_update = {
+    'untouched': 0,
+    'changed': 0,
+    'error': 0,
+}
 
 total = db[COL].count()
 
@@ -127,78 +253,46 @@ for doc in db[COL].find().sort([('_id.id', 1), ('_id.type', -1), ('_id.servicePa
     # It may happen that entity doesn't have any attribute. We early detect that situation and skip in that case
     if len(doc[ATTRS].keys()) == 0:
         #print '- %d: entity without attributes %s. Skipping' % (processed, json.dumps(doc['_id']))
-        untouched += 1
+        counter_analysis['no'] += 1
+        counter_update['untouched'] += 1
         continue   # entities loop
 
-    # Processing attribute by attribute
-    to_skip = True
-    new_attrs = dict(doc[ATTRS])
-    new_attrnames = list(doc[ATTRNAMES])
-    check_attrs = []
+    attrs_with_md_id = extract_attr_with_md_id(doc[ATTRS])
+    if len(attrs_with_md_id.keys()) > 0:
+        print '- {}: entity {} ({}): metadata ID detected'.format(processed, json.dumps(doc['_id']),
+                                                                  date2string(doc['modDate']))
 
-    for attr in doc[ATTRS].keys():
+        max_number_of_ids = 0
+        for attr in attrs_with_md_id.keys():
+            print '     {}: [ {} ]'.format(attr, ', '.join(attrs_with_md_id[attr]))
+            if len(attrs_with_md_id[attr]) > max_number_of_ids:
+                max_number_of_ids = len(attrs_with_md_id[attr])
 
-        if attr.find("()") > 0:
-            # Note that Orion <=1.13.0 doesn't allows attributes with same name with ID and not ID at the same time
-            # in the same entity, so we don't need to take into account that situation.
-
-            tokens = attr.split("()")
-            name = tokens[0]
-            id = tokens[1]
-            print "- entity {}: found attr <{}>, metadata id <{}>, value <{}>".format(doc['_id'], name, id,
-                                                                                      doc[ATTRS][attr]['value'])
-
-            if autofix:
-                # Prepare modification in new_attrs and new_attrnames
-
-                # Composing new name, eg. temperature:ID1
-                new_name = name + ':' + id
-
-                # Remove all name from attrNames list (e.g. temperature)...
-                if name in new_attrnames:
-                    new_attrnames.remove(name)
-
-                # ...and add the new one (temperature:ID1). Previous check of temperature:ID1 is not already
-                # in the list: that will be an unsolvable situation
-                if new_name in new_attrnames:
-                    print "- ERROR: attribute <{}> already exist in entity {}. Cannot be automatically fixed".format(new_name, doc['_id'])
-                    need_fix = True
-                else:
-                    new_attrnames.append(new_name)
-
-                    # Changing temperature()ID1 for temperature:ID1 in the attrs key-map
-                    new_attrs[new_name] = new_attrs.pop(attr)
-
-                    check_attrs.append(new_name)
-
-                    to_skip = False
-
-    # Need to skip to next entity? Note that if autofix==False to_skip is always True
-    if to_skip:
-        if need_fix:
-            error += 1
+        if max_number_of_ids > 1:
+            counter_analysis['dup'] += 1
+            autofix_as_metadata_not_possible = True
         else:
-            untouched += 1
-        continue    # entities loop 
-
-    # Update document with the new attribute fields
-    db[COL].update(flatten(doc['_id']), {'$set': {ATTRS: new_attrs, ATTRNAMES: new_attrnames}})
-
-    # Check update was ok (this is not an exhaustive checking that is better than nothing :)
-    check_doc = db[COL].find_one(flatten(doc['_id']))
-
-    if update_ok(check_doc, check_attrs):
-        changed += 1
+            counter_analysis['single'] += 1
     else:
-        print "- %d: ERROR: document <%s> change attempt failed!" % (processed, json.dumps(check_doc['_id']))
+        counter_analysis['no'] += 1
+        counter_update['untouched'] += 1
+        continue  # entities loop
+
+    # Try to fix the entity
+    result = fix_entity(doc, attrs_with_md_id)
+    counter_update[result] += 1
+    if result == 'error':
         need_fix = True
-        error += 1
 
 print '- processing entity: %d/%d' % (processed, total)
-print '- documents processed:   %d' % processed
-print '  * changed:             %d' % changed
-print '  * untouched:           %d' % untouched
-print '  * error:               %d' % error
+print '- documents analyzed:                                                 %d' % processed
+print '  * entities w/o any attr w/ md ID:                                   %d' % counter_analysis['no']
+print '  * entities w/ at least one attr w/ more than one ID:                %d' % counter_analysis['dup']
+print '  * entities w/ at least one attr w/ md ID all them w/ single md ID:  %d' % counter_analysis['single']
+print '- documents processed:                                                %d' % processed
+print '  * untouched:                                                        %d' % counter_update['untouched']
+print '  * changed: entities w/ at least one attr w/ more than one ID:       %d' % counter_update['changed']
+print '  * attempt to change but error:                                      %d' % counter_update['error']
 
 if need_fix:
     print "------------------------------------------------------"
