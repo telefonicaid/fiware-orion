4337dcd16f67445f91e6a58a5d6cc4a5c1f8a508
Ken Zangelin <kzangeli@gmail.com>
2019-05-03 15:53:21 +0200

    Correct git version

--- src/lib/orionld/serviceRoutines/orionldGetVersion.cpp
+++ src/lib/orionld/serviceRoutines/orionldGetVersion.cpp
@@ -50,7 +50,7 @@ bool orionldGetVersion(ConnectionInfo* ciP)
 
   ciP->responseTree = kjObject(orionldState.kjsonP, NULL);
 
-  nodeP = kjString(orionldState.kjsonP, "branch", "bug/ngsild_issue_0043");
+  nodeP = kjString(orionldState.kjsonP, "branch", "bug/125.orionld_issue_0033");
   kjChildAdd(ciP->responseTree, nodeP);
 
   nodeP = kjString(orionldState.kjsonP, "kbase version", kbaseVersion);
