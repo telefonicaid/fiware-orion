bd3e2a47aff57e707d0cb30e68bc20dfddbdddd6
Ken Zangelin <kzangeli@gmail.com>
2019-05-07 12:33:35 +0200

    Functest for issue #27

--- src/lib/orionld/serviceRoutines/orionldGetVersion.cpp
+++ src/lib/orionld/serviceRoutines/orionldGetVersion.cpp
@@ -50,7 +50,7 @@ bool orionldGetVersion(ConnectionInfo* ciP)
 
   ciP->responseTree = kjObject(orionldState.kjsonP, NULL);
 
-  nodeP = kjString(orionldState.kjsonP, "branch", "bug/131.orionld_issue_0028");
+  nodeP = kjString(orionldState.kjsonP, "branch", "bug/132.orionld_issue_0027");
   kjChildAdd(ciP->responseTree, nodeP);
 
   nodeP = kjString(orionldState.kjsonP, "kbase version", kbaseVersion);
