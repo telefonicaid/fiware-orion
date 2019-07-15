5683b1211b93adeff4420acacb0fe9176ef7b2eb
Ken Zangelin <kzangeli@gmail.com>
2019-07-15 10:10:50 +0200

    Fixed an ugly bug about memory corruption in contexts

--- src/lib/orionld/context/orionldContextAdd.cpp
+++ src/lib/orionld/context/orionldContextAdd.cpp
@@ -29,6 +29,7 @@ extern "C"
 {
 #include "kjson/KjNode.h"                                      // KjNode
 #include "kjson/kjParse.h"                                     // kjParse
+#include "kjson/kjClone.h"                                     // kjClone
 }
 
 #include "rest/ConnectionInfo.h"                               // ConnectionInfo
@@ -128,7 +129,7 @@ OrionldContext* orionldContextAdd
     return contextP;
   }
 
-  LM_T(LmtContext, ("Downloading and parsing context of URL '%s'", url));
+  LM_TMP(("KZ: Calling orionldContextDownloadAndParse('%s')", url));
   KjNode* tree = orionldContextDownloadAndParse(kjsonP, url, true, detailsPP);
 
   if (tree == NULL)
@@ -202,6 +203,8 @@ OrionldContext* orionldContextAdd
   // - an object with key-values (a "leaf")
   // - a vector of contexts (URL strings)
   //
+  LM_TMP(("KZ: Cloning context '%s'", url));
+  tree = kjClone(tree);
 
   if ((contextP = orionldContextAppend(url, tree, contextType, detailsPP)) == NULL)
     return NULL;
@@ -267,6 +270,7 @@ OrionldContext* orionldContextAdd
     if (orionldContextLookup(url) != NULL)
       continue;
 
+    LM_TMP(("KZ: Calling orionldContextDownloadAndParse(%s)", url));
     tree = orionldContextDownloadAndParse(orionldState.kjsonP, url, true, detailsPP);
     if (tree == NULL)
     {
@@ -277,7 +281,7 @@ OrionldContext* orionldContextAdd
 
       return NULL;
     }
-
+    tree = kjClone(tree);
     if (orionldContextAppend(url, tree, contextType, detailsPP) == NULL)
     {
       LM_T(LmtContext, (*detailsPP));
