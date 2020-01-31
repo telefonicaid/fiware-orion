ab67a2ecca1210588b1ce9edc7de84793bef413f
Fermín Galán Márquez <fermin.galanmarquez@telefonica.com>
2020-01-31 10:39:26 +0100

    FIX lint error

--- src/lib/mongoBackend/mongoQueryTypes.cpp
+++ src/lib/mongoBackend/mongoQueryTypes.cpp
@@ -35,7 +35,7 @@
 
 #include "mongoBackend/MongoGlobal.h"
 #include "mongoBackend/connectionOperations.h"
-#include "mongoConnectionPool.h"
+#include "mongoBackend/mongoConnectionPool.h"
 #include "mongoBackend/dbFieldEncoding.h"
 #include "mongoBackend/safeMongo.h"
 #include "mongoBackend/mongoQueryTypes.h"
