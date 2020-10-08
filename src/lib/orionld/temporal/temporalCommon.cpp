b964e58bdc25501a1121ff4d64b202cc9150d8c5
Chandra Challagonda <challago@gmail.com>
2020-10-08 14:31:01 +0300

    from local comp 8.10.2020 - part 3

--- src/lib/orionld/temporal/temporalCommon.cpp
+++ src/lib/orionld/temporal/temporalCommon.cpp
@@ -162,7 +162,7 @@ OrionldTemporalDbAllTables*  singleTemporalEntityExtract()
     for (KjNode* attrP = orionldState.requestTree->value.firstChildP; attrP != NULL; attrP = attrP->next)
     {
        dbAttributeTableLocal[attrIndex].entityId = dbEntityTableLocal[0].entityId;
-       attrExtract (attrP, &dbAttributeTableLocal[attrIndex], dbSubAttributeTableLocal[attrIndex], attrIndex);
+       attrExtract (attrP, &dbAttributeTableLocal[attrIndex], dbSubAttributeTableLocal, attrIndex);
        attrIndex++;
     }
 
@@ -326,7 +326,7 @@ OrionldTemporalDbAllTables*  singleTemporalEntityExtract()
 }
 
 
-void  attrExtract(KjNode* attrP, OrionldTemporalDbAttributeTable* dbAttributeTableLocal, OrionldTemporalDbSubAttributeTable* dbSubAttributeTableLocal, int attrIndex)
+void  attrExtract(KjNode* attrP, OrionldTemporalDbAttributeTable* dbAttributeTableLocal, OrionldTemporalDbSubAttributeTable** dbSubAttributeTableLocal, int attrIndex)
 {
    //int oldTemporalTreeNodeLevel = 0;
    //for (KjNode* attrP = orionldState.requestTree->value.firstChildP; attrP != NULL; attrP = attrP->next)
