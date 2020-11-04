ba6bbe0d794eb159e3cff69d2eb206af7a379154
Stefan Wiedemann <wistefan@googlemail.com>
2020-11-04 09:53:00 +0100

    change log level

--- src/app/contextBroker/contextBroker.cpp
+++ src/app/contextBroker/contextBroker.cpp
@@ -971,12 +971,12 @@ int main(int argC, char* argV[])
   //
   if (paIsSet(argC, argV, "-disableFileLog"))
   {
-    LM_I(("File log disabled"));
+    LM_X(("File log disabled"));
     paConfig("log to file",                   (void*) false);
   } 
   else
   {
-    LM_I(("File log enabled"));
+    LM_X(("File log enabled"));
     paConfig("log to file",                   (void*) true);
   }
 
