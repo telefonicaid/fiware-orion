33d340fcc78826e51ffa44ac5357522ca975b54a
Chandra Challagonda <challago@gmail.com>
2020-11-29 13:19:02 +0100

    FromLocalComputer29112020-part15

--- src/lib/orionld/temporal/temporalCommon.cpp
+++ src/lib/orionld/temporal/temporalCommon.cpp
@@ -118,8 +118,8 @@ void entityExtract (OrionldTemporalDbAllTables* allTab, KjNode* entityP, bool ar
 
     LM_TMP (("CCSR : entityExtract %i", entityIndex));
 
-    allTab->entityTableArray[entityIndex].entityId = idP.payloadIdNode->value.s;
-    allTab->entityTableArray[entityIndex].entityType = (typeP != NULL)? typeP.payloadIdNode->value.s : NULL;
+    allTab->entityTableArray[entityIndex].entityId = idP->payloadIdNode->value.s;
+    allTab->entityTableArray[entityIndex].entityType = (typeP != NULL)? typeP->payloadIdNode->value.s : NULL;
     kjChildRemove (entityP, idP);
     if (typeP != NULL)
       kjChildRemove(entityP, typeP);
