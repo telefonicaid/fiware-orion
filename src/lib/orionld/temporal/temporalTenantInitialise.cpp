a0eb8b6212d01436c9c6614dd9d9f0cee8d25193
Chandra Challagonda <challago@gmail.com>
2020-12-01 23:37:42 +0100

    database actions changes From the local computer 01122020 Part 24

--- src/lib/orionld/temporal/temporalTenantInitialise.cpp
+++ src/lib/orionld/temporal/temporalTenantInitialise.cpp
@@ -212,9 +212,9 @@ bool temporalTenantInitialise(const char* tenant)
       // need to create a routine - Fix me PLEEEEEASE - start
       PGresult *res = PQexec(oldPgDbTenantConnection,
         "SELECT entity_id FROM entity_table");
-        
+
       if(PQresultStatus(res)==PGRES_FATAL_ERROR &&
-        strstr(PQresultErrorMessage(res),"Relation \"mytable\" does not exist")
+        strstr(PQresultErrorMessage(res),"Relation \"mytable\" does not exist"))
         {
           LM_K(("CCSR - Table does not EXIST - what can we do"));
         }
