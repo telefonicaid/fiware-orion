8a314d681413fe2f7e05d3d3fd2d64ec4f999d5e
Stefan Wiedemann <wistefan@googlemail.com>
2020-12-08 15:51:01 +0100

    use multistaged build create base image deploy debug and release image

---
+++ docker/build/install-build-dependencies.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Copyright 2014 Telefonica Investigacion y Desarrollo, S.A.U
+#
+# This file is part of Orion Context Broker.
+#
+# Orion Context Broker is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Orion Context Broker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Orion Context Broker. If not, see http://www.gnu.org/licenses/.
+#
+# For those usages not covered by this license please contact with
+# iot_support at tid dot es
+
+set -e
+
+BUILD_DEPS=(
+ 'libboost-all-dev' \
+ 'libcurl4-openssl-dev' \
+ 'libgcrypt-dev' \
+ 'libgnutls28-dev' \
+ 'libsasl2-dev' \
+ 'libssl-dev' \
+ 'uuid-dev' \
+)
+
+BUILD_TOOLS=(
+ 'apt-transport-https' \
+ 'bzip2' \
+ 'ca-certificates' \
+ 'cmake' \
+ 'curl' \
+ 'dirmngr' \
+ 'g++' \
+ 'gcc' \
+ 'git' \
+ 'gnupg' \
+ 'make' \
+ 'scons' \
+)
+
+apt-get -y install -f --no-install-recommends \
+    ${BUILD_TOOLS[@]} \
+    ${BUILD_DEPS[@]}
