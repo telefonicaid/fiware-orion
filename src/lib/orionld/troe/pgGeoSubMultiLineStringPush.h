eb0e8b69206c819b9cebaa80045dfb7d55ee0903
Ken Zangelin <kzangeli@gmail.com>
2021-01-11 11:37:32 +0100

    GeoJSON MultiLineString in TRoE for Sub-GeoProperties

---
+++ src/lib/orionld/troe/pgGeoSubMultiLineStringPush.h
@@ -0,0 +1,56 @@
+#ifndef SRC_LIB_ORIONLD_TROE_PGGEOSUBMULTILINESTRINGPUSH_H_
+#define SRC_LIB_ORIONLD_TROE_PGGEOSUBMULTILINESTRINGPUSH_H_
+
+/*
+*
+* Copyright 2021 FIWARE Foundation e.V.
+*
+* This file is part of Orion-LD Context Broker.
+*
+* Orion-LD Context Broker is free software: you can redistribute it and/or
+* modify it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* Orion-LD Context Broker is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+* General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Orion-LD Context Broker. If not, see http://www.gnu.org/licenses/.
+*
+* For those usages not covered by this license please contact with
+* orionld at fiware dot org
+*
+* Author: Ken Zangelin
+*/
+#include <postgresql/libpq-fe.h>                                 // PGconn
+
+extern "C"
+{
+#include "kjson/KjNode.h"                                        // KjNode
+}
+
+
+
+// -----------------------------------------------------------------------------
+//
+// pgGeoSubMultiLineStringPush - push a Geo-MultiLineString subproperty to its DB table
+//
+extern bool pgGeoSubMultiLineStringPush
+(
+  PGconn*      connectionP,
+  KjNode*      coordinatesP,
+  const char*  instanceId,
+  const char*  entityRef,
+  const char*  entityId,
+  const char*  attributeRef,
+  const char*  attributeId,
+  const char*  subAttributeName,
+  const char*  observedAt,
+  const char*  createdAt,
+  const char*  modifiedAt
+);
+
+#endif  // SRC_LIB_ORIONLD_TROE_PGGEOSUBMULTILINESTRINGPUSH_H_
