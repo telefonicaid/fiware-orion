1c15672043152aa5cfa327c0017d868159a3fc98
Ken Zangelin <kzangeli@gmail.com>
2021-09-01 00:28:46 +0200

    Added some traces plus grep of them in functest to find why the test fails in github actions

--- src/lib/mongoBackend/MongoCommonUpdate.cpp
+++ src/lib/mongoBackend/MongoCommonUpdate.cpp
@@ -499,6 +499,14 @@ static bool compoundValueDiffers(CompoundValueNode* newValueP, mongo::BSONObj* o
 
   bool differs = strcmp(bufOld, bufNew) != 0;
 
+  LM_TMP(("CDIF: Sorted old tree: %s", bufOld));
+  LM_TMP(("CDIF: Sorted new tree: %s", bufNew));
+
+  if (differs == true)
+    LM_TMP(("CDIF: the trees differ"));
+  else
+    LM_TMP(("CDIF: the trees are equal"));
+
   return differs;
 }
 
@@ -734,6 +742,7 @@ static bool mergeAttrInfo(const BSONObj& attr, ContextAttribute* caP, BSONObj* m
     mongo::BSONArray  arrayValue;
 
     bool isArray = false;
+    LM_TMP(("oldValueType: %d", oldValueType));
     if (oldValueType == 4)
     {
       isArray = true;
