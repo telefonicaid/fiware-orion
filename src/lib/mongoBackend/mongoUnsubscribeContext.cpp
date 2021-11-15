58469edb28b166b8fbf80c01f7e91f7f0fd553b6
Anjali Pathak <anjali.pathak@india.nec.com>
2021-11-15 14:13:01 +0530

    Update mongoUnsubscribeContext.cpp

--- src/lib/mongoBackend/mongoUnsubscribeContext.cpp
+++ src/lib/mongoBackend/mongoUnsubscribeContext.cpp
@@ -141,8 +141,10 @@ HttpStatusCode mongoUnsubscribeContext
     {
       subCacheItemRemove(cSubP);
     }
-  cacheSemGive(__FUNCTION__, "Removing subscription from cache");
+
+    cacheSemGive(__FUNCTION__, "Removing subscription from cache");
   }
+
   reqSemGive(__FUNCTION__, "ngsi10 unsubscribe request", reqSemTaken);
   responseP->statusCode.fill(SccOk);
 
