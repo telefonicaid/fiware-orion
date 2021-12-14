875d465fd10b37ef6f13a394daa16a4304c8435a
Stefan Wiedemann <wistefan@googlemail.com>
2021-12-14 14:02:55 +0100

    lower case

--- .github/build-and-push-rpm.sh
+++ .github/build-and-push-rpm.sh
@@ -6,6 +6,6 @@ docker run -e BROKER_RELEASE=nightly -v $(pwd):/opt/orion --workdir=/opt/orion f
 
 file_list=$(ls $(pwd)/rpm/RPMS/x86_64/)
 
-for(String file : FILES_LIST.split("\\r?\\n")){
+for(String file : file_list.split("\\r?\\n")){
   curl -v -f -u telefonica-github:${{ secrets.NEXUS_PASSWORD }} --upload-file $(pwd)/rpm/RPMS/x86_64/${file} https://nexus.lab.fiware.org/repository/el/8/x86_64/nightly/${file}
 }
