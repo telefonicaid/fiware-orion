b3388b175b8ddf1f5389684a0c08a5dcf47d554e
Stefan Wiedemann <wistefan@googlemail.com>
2021-12-14 14:12:56 +0100

    change loop

--- .github/build-and-push-rpm.sh
+++ .github/build-and-push-rpm.sh
@@ -4,8 +4,8 @@ set -e
 
 docker run -e BROKER_RELEASE=nightly -v $(pwd):/opt/orion --workdir=/opt/orion fiware/orion-ci:rpm8 make rpm
 
-file_list=$(ls $(pwd)/rpm/RPMS/x86_64/)
-
-for(String file : file_list.split("\\r?\\n")){
-  curl -v -f -u telefonica-github:${{ secrets.NEXUS_PASSWORD }} --upload-file $(pwd)/rpm/RPMS/x86_64/${file} https://nexus.lab.fiware.org/repository/el/8/x86_64/nightly/${file}
-}
+for file in "$(pwd)/rpm/RPMS/x86_64/*"
+do
+  echo "Processing $file file..."
+  curl -v -f -u telefonica-github:${{ secrets.NEXUS_PASSWORD }} --upload-file $(pwd)/rpm/RPMS/x86_64/$file https://nexus.lab.fiware.org/repository/el/8/x86_64/nightly/$file
+done
