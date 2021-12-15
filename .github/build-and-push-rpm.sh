091ce780740094f5997c600b4c2832f9728973f0
Stefan Wiedemann <wistefan@googlemail.com>
2021-12-15 08:05:14 +0100

    take basename

--- .github/build-and-push-rpm.sh
+++ .github/build-and-push-rpm.sh
@@ -6,6 +6,7 @@ docker run -e BROKER_RELEASE=nightly -v $(pwd):/opt/orion --workdir=/opt/orion f
 
 for file in "$(pwd)/rpm/RPMS/x86_64"/*
 do
-  echo "Processing $file file..."
-  curl -v -f -u telefonica-github:$1 --upload-file $(pwd)/rpm/RPMS/x86_64/$file https://nexus.lab.fiware.org/repository/el/8/x86_64/nightly/$file
+  filename=$(basename file)
+  echo "Processing $filename file..."
+  curl -v -f -u telefonica-github:$1 --upload-file $file https://nexus.lab.fiware.org/repository/el/8/x86_64/nightly/$filename
 done
