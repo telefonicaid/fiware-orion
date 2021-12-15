ba8e9fcaa86ef648d83d44b1c9e58568b0a73201
Stefan Wiedemann <wistefan@googlemail.com>
2021-12-15 08:11:13 +0100

    fix file var

--- .github/build-and-push-rpm.sh
+++ .github/build-and-push-rpm.sh
@@ -6,7 +6,7 @@ docker run -e BROKER_RELEASE=nightly -v $(pwd):/opt/orion --workdir=/opt/orion f
 
 for file in "$(pwd)/rpm/RPMS/x86_64"/*
 do
-  filename=$(basename file)
+  filename=$(basename $file)
   echo "Processing $filename file..."
   curl -v -f -u telefonica-github:$1 --upload-file $file https://nexus.lab.fiware.org/repository/el/8/x86_64/nightly/$filename
 done
