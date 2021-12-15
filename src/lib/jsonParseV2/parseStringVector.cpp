2ef28abbb6a0d860fbda922993fdc48c606bbbaa
Änjali <Anjali.Pathak@india.nec.com>
2021-12-15 15:24:12 +0000

    Updated code

--- src/lib/jsonParseV2/parseStringVector.cpp
+++ src/lib/jsonParseV2/parseStringVector.cpp
@@ -71,13 +71,13 @@ bool parseStringVector
 
    for (rapidjson::Value::ConstValueIterator iter_new = iter+1; iter_new != jsonVector.End(); ++iter_new)
    {
-    std::string val = iter_new->GetString();
+     std::string val = iter_new->GetString();
 
-    if (value == val)
-    {
+     if (value == val)
+     {
        *errorStringP = "duplicated attribute /" + value + "/";
        return false;
-    }
+     }
    }
 
     if (forbiddenIdCharsV2(value.c_str()))
