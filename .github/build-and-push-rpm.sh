02932927e25634a0985dece84596b3f0893ac581
Stefan Wiedemann <wistefan@googlemail.com>
2021-12-15 10:09:24 +0100

    add support for nightly and release

--- .github/build-and-push-rpm.sh
+++ .github/build-and-push-rpm.sh
@@ -2,11 +2,14 @@
 
 set -e
 
-docker run -e BROKER_RELEASE=nightly -v $(pwd):/opt/orion --workdir=/opt/orion fiware/orion-ci:rpm8 make rpm
+secret=$1
+type=$2
+
+docker run -e BROKER_RELEASE=$type -v $(pwd):/opt/orion --workdir=/opt/orion fiware/orion-ci:rpm8 make rpm
 
 for file in "$(pwd)/rpm/RPMS/x86_64"/*
 do
   filename=$(basename $file)
-  echo "Processing $filename file..."
-  curl -v -f -u telefonica-github:$1 --upload-file $file https://nexus.lab.fiware.org/repository/el/8/x86_64/nightly/$filename
+  echo "Uploading $filename"
+  curl -v -f -u telefonica-github:$secret --upload-file $file https://nexus.lab.fiware.org/repository/el/8/x86_64/$type/$filename
 done
