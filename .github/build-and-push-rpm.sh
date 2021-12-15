03d6aae0b3f06e3f16be5041d6863bf4aa19c788
Stefan Wiedemann <wistefan@googlemail.com>
2021-12-15 07:53:14 +0100

    use
    pass secret as argument

--- .github/build-and-push-rpm.sh
+++ .github/build-and-push-rpm.sh
@@ -7,5 +7,5 @@ docker run -e BROKER_RELEASE=nightly -v $(pwd):/opt/orion --workdir=/opt/orion f
 for file in "$(pwd)/rpm/RPMS/x86_64/*"
 do
   echo "Processing $file file..."
-  curl -v -f -u telefonica-github:${{ secrets.NEXUS_PASSWORD }} --upload-file $(pwd)/rpm/RPMS/x86_64/$file https://nexus.lab.fiware.org/repository/el/8/x86_64/nightly/$file
+  curl -v -f -u telefonica-github:$1 --upload-file $(pwd)/rpm/RPMS/x86_64/$file https://nexus.lab.fiware.org/repository/el/8/x86_64/nightly/$file
 done
