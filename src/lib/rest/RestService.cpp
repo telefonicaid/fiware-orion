80f47ae1e528a5f13f3ce59d1bf7ffe61580da63
Ken Zangelin <kzangeli@gmail.com>
2021-12-28 16:19:18 +0100

    Fixed a functest

--- src/lib/rest/RestService.cpp
+++ src/lib/rest/RestService.cpp
@@ -540,6 +540,7 @@ std::string restService(ConnectionInfo* ciP, RestService* serviceV)
     OrionError  error(SccBadRequest, "The Orion Context Broker is a REST service, not a 'web page'");
     std::string response = error.render();
 
+    ciP->httpStatusCode = SccBadRequest;
     alarmMgr.badInput(clientIp, "The Orion Context Broker is a REST service, not a 'web page'");
     restReply(ciP, response);
 
