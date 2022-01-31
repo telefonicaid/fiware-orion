0b5b147927879f3005d1e7357d7d709749fbb58e
mapedraza <40356341+mapedraza@users.noreply.github.com>
2022-01-31 17:19:40 +0100

    escape `+` characters from sed

--- .github/build-and-push-rpm.sh
+++ .github/build-and-push-rpm.sh
@@ -19,7 +19,7 @@ do
   then
       # to make the name of the .rpm independent of the version name (i.e. contextBroker-nightly.x86_64.rpm),
       # avoiding obsolete packages to remain in the yum version when version number steps
-      filename=$(echo $filename | sed 's/[0-9]+.[0-9]+.[0-9]+_next-//g')
+      filename=$(echo $filename | sed 's/[0-9]\+.[0-9]\+.[0-9]\+_next-//g')
   fi
   echo "Uploading $filename"
   curl -v -f -u telefonica-github:$secret --upload-file $file https://nexus.lab.fiware.org/repository/el/8/x86_64/$type/$filename
