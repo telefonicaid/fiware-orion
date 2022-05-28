677594b4adb0d14ce582eb50c1855f97309ba245
Ken Zangelin <kzangeli@gmail.com>
2022-05-29 00:57:42 +0200

    New CLI: -mongocOnly, for accepting ONLY requests that are implemented using mongoc

--- src/lib/mongoBackend/MongoGlobal.cpp
+++ src/lib/mongoBackend/MongoGlobal.cpp
@@ -228,10 +228,6 @@ void mongoInit
   bool         mutexTimeStat
 )
 {
-  extern bool experimental2;
-  if (experimental2 == true)
-    LM_X(1, ("In mongoInit with -experimental2 set"));
-
   double tmo = timeout / 1000.0;  // milliseconds to float value in seconds
   if (!mongoStart(dbHost, dbName.c_str(), rplSet, user, pwd, mtenant, tmo, writeConcern, dbPoolSize, mutexTimeStat))
   {
@@ -412,9 +408,6 @@ DBClientBase* getMongoConnection(void)
 #ifdef UNIT_TEST
   return connection;
 #else
-  extern bool experimental2;
-  if (experimental2 == true)
-    LM_X(1, ("In getMongoConnection with -experimental2 set"));
 
 #ifdef REQUEST_PERFORMANCE
   DBClientBase*     connectionP;
--- src/lib/orionld/common/orionldState.h
+++ src/lib/orionld/common/orionldState.h
@@ -556,6 +556,7 @@ extern bool              idIndex;                  // From orionld.cpp
 extern bool              noNotifyFalseUpdate;      // From orionld.cpp
 extern char              mongoServerVersion[32];
 extern bool              experimental;             // From orionld.cpp
+extern bool              mongocOnly;               // From orionld.cpp
 extern char              allowedOrigin[64];        // From orionld.cpp (CORS)
 extern int               maxAge;                   // From orionld.cpp (CORS)
 extern char              userAgentHeader[64];      // From notificationSend.cpp
