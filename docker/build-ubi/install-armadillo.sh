1e39dfb33b1353e6edaba379e6c1491ca544bac2
Ken Zangelin <kzangeli@gmail.com>
2022-06-20 18:56:17 +0200

    installing all 5 packages from downloaded RPM - for the error in postgres installation about 404 for armadillo

--- docker/build-ubi/install-armadillo.sh
+++ docker/build-ubi/install-armadillo.sh
@@ -23,20 +23,43 @@
 curl -L -v -O ftp://ftp.pbone.net/mirror/download.fedora.redhat.com/pub/fedora/epel/7/x86_64/Packages/s/SuperLU-5.2.0-5.el7.x86_64.rpm
 if [ -f SuperLU-5.2.0-5.el7.x86_64.rpm ]
 then
-    yum localinstall SuperLU-5.2.0-5.el7.x86_64.rpm
+    yum localinstall -y SuperLU-5.2.0-5.el7.x86_64.rpm
 else
     echo Error downloading SuperLU-5.2.0-5.el7.x86_64.rpm
 fi
 
 
-#yum install -y arpack
-#yum install -y hdf5-devel
-#yum install -y lapack-devel
+curl -v -L -O https://kojipkgs.fedoraproject.org//packages/arpack/3.8.0/4.fc36/src/arpack-3.8.0-4.fc36.src.rpm
+if [ -f arpack-3.8.0-4.fc36.src.rpm ]
+then
+    yum localinstall -y arpack-3.8.0-4.fc36.src.rpm
+else
+    echo Error downloading arpack-3.8.0-4.fc36.src.rpm
+fi
+
+
+curl -v -L -O http://rpmfind.net/linux/opensuse/tumbleweed/repo/oss/x86_64/libhdf5-103-32bit-1.10.8-5.2.x86_64.rpm
+if [ -f libhdf5-103-32bit-1.10.8-5.2.x86_64.rpm ]
+then
+    yum localinstall -y libhdf5-103-32bit-1.10.8-5.2.x86_64.rpm
+else
+    echo Error downloading libhdf5-103-32bit-1.10.8-5.2.x86_64.rpm
+fi
+
+
+curl -v -L -O http://rpmfind.net/linux/fedora/linux/releases/36/Everything/x86_64/os/Packages/l/lapack-3.10.0-5.fc36.i686.rpm
+if [ -f lapack-3.10.0-5.fc36.i686.rpm ]
+then
+    yum localinstall -y lapack-3.10.0-5.fc36.i686.rpm
+else
+    echo Error downloading lapack-3.10.0-5.fc36.i686.rpm
+fi
+
 
 curl -v -L -O http://yum.stanford.edu/mrepo/epel-EL8-x86_64/RPMS.all/armadillo-9.700.2-1.el8.x86_64.rpm
 if [ -f armadillo-9.700.2-1.el8.x86_64.rpm ]
 then
-    yum localinstall armadillo-9.700.2-1.el8.x86_64.rpm
+    yum localinstall -y armadillo-9.700.2-1.el8.x86_64.rpm
 else
     echo Error downloading armadillo-9.700.2-1.el8.x86_64.rpm
 fi
