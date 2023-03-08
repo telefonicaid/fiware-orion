87b8a13a499a5b21542023b0c705faadb945ab2c
Fermín Galán Márquez <fermin.galanmarquez@telefonica.com>
2023-03-08 16:38:00 +0100

    FIX initialize local var in lmTraceGet function

--- src/lib/logMsg/logMsg.cpp
+++ src/lib/logMsg/logMsg.cpp
@@ -1520,7 +1520,7 @@ LmStatus lmTraceSub(const char* levelFormat)
 */
 char* lmTraceGet(char* levelString, int levelStringSize)
 {
-  int       i;
+  int       i = 0;
   int       j = 0;
   int       levels[256];
 
@@ -1596,7 +1596,7 @@ char* lmTraceGet(char* levelString, int levelStringSize)
 */
 char* lmTraceGet(char* levelString, int levelStringSize, char* traceV)
 {
-  int       i;
+  int       i = 0;
   int       j = 0;
   int       levels[256];
 
