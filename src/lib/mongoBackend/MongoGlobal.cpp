ce0f75dbc317bdff45aae449608962b08156fbae
ArqamFarooqui110719 <md.farooqui@india.nec.com>
2023-03-15 11:26:11 +0000

    Fix service name too long at subCacheRefresh issue#2848

--- src/lib/mongoBackend/MongoGlobal.cpp
+++ src/lib/mongoBackend/MongoGlobal.cpp
@@ -251,9 +251,17 @@ bool getOrionDatabases(std::vector<std::string>* dbsP)
 
     if (strncmp(prefix.c_str(), dbName.c_str(), strlen(prefix.c_str())) == 0)
     {
-      LM_T(LmtMongo, ("Orion database found: %s", dbName.c_str()));
-      dbsP->push_back(dbName);
-      LM_T(LmtBug, ("Pushed back db name '%s'", dbName.c_str()));
+      if (strlen(dbName.c_str()) <= 50) //Check for size of database name, db names size should be <= '50' bytes.
+      {
+        LM_T(LmtMongo, ("Orion database found: %s", dbName.c_str()));
+        dbsP->push_back(dbName);
+        LM_T(LmtBug, ("Pushed back db name '%s'", dbName.c_str()));
+      }
+      else
+      {
+        LM_E(("Runtime Error (database name size should be smaller than 50 characters: %s)", dbName.c_str()));
+	return false;
+      }
     }
   }
 
