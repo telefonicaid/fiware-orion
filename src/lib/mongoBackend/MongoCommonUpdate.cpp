f0e788f54b23e9c7d6b8e6f2abc1d85dfc32452f
Fermín Galán Márquez <fermin.galanmarquez@telefonica.com>
2023-05-08 11:47:56 +0200

    FIX $unset update operator not working properly with keys with dots

--- src/lib/mongoBackend/MongoCommonUpdate.cpp
+++ src/lib/mongoBackend/MongoCommonUpdate.cpp
@@ -3471,7 +3471,9 @@ static bool calculateUnsetOperator(ContextElementResponse* cerP, orion::BSONObjB
     {
       CompoundValueNode* child = theChild->childV[jx];
 
-      std::string valueKey = baseKey + "." + child->name;
+      // dbEncode is needed, in order to avoid problems as the one
+      // described in issue #4315
+      std::string valueKey = baseKey + "." + dbEncode(child->name);
       b->append(valueKey, 1);
     }
   }
