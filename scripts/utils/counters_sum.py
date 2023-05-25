1bf8f01a699a9729c650e6acd96beff4a18b66ac
Fermín Galán Márquez <fgalan@users.noreply.github.com>
2023-05-25 16:32:46 +0200

    Create counters_sum.py

---
+++ scripts/utils/counters_sum.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2023 Telefonica Investigacion y Desarrollo, S.A.U
+#
+# This file is part of Orion Context Broker.
+#
+# Orion Context Broker is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Orion Context Broker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Orion Context Broker. If not, see http://www.gnu.org/licenses/.
+#
+# For those usages not covered by this license please contact with
+# iot_support at tid dot es
+
+# Usage:
+#   counters_sum.py file1.json file2.json ... fileN.json
+#
+# (Maybe you need to use 'PYTHONIOENCODING=utf8 python counter_sum.py ...' if you are going to redirect the output of this
+# script to a file)
+
+# This script is designed to aggregate information coming of several CB instances running in parallel in HA or
+# horizontal scaling scenarios. The files passed to the script as arguments are the response of the
+# operation GET /v2/statistics?options=fullCounters
+#
+# Some notes to take into account:
+#
+# - All .json files have to have a regular structure (i.e. all with the same structural keys in the JSON object)
+#   Thus, it is a must to use ?option=fullCounter in the GET operation
+# - At the present moment this script only aggregates information in the "counters" section, but it would be
+#   easily extended to take into account other sections
+
+__author__ = 'fermin'
+
+import sys
+import json
+
+# Grab all files into JSON dicts
+files = []
+for f in sys.argv[1:]:
+    files.append(json.load(open(f)))
+
+# Initialize accumulator with first file
+accum = files[0]['counters']
+
+# Accum the rest of the files
+for f in files[1:]:
+    counters = f['counters']
+
+    # Update summaries
+    summary_fields = [
+        'jsonRequests',
+        'textRequests',
+        'noPayloadRequests',
+        'missedVerb',
+        'invalidRequests',
+        'registrationUpdateErrors',
+        'discoveryErrors',
+        'notificationsSent'
+    ]
+
+    for field in summary_fields:
+        accum[field] += counters[field]
+
+    # Process request blocks
+    for field in ['requests', 'requestsLegacy']:
+        for url in accum[field]:
+            for verb in accum[field][url]:
+                accum[field][url][verb] += counters[field][url][verb]
+
+print(json.dumps(accum))
