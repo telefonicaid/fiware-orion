42f215efc30fc2b5faf1c8ad53051426704c1edb
Fermín Galán Márquez <fermin.galanmarquez@telefonica.com>
2024-02-05 16:51:59 +0100

    FIX PoC to measure times

--- src/lib/jexl/JexlManager.cpp
+++ src/lib/jexl/JexlManager.cpp
@@ -40,6 +40,44 @@ void JexlManager::init(void)
   }
 
   // FIXME PR: Py_Finalize() has to be be called in the proper place
+  LM_T(LmtJexl, ("Python interpreter is about to be initialized"));
   Py_Initialize();
   LM_T(LmtJexl, ("Python interpreter has been initialized"));
+
+  // FIXME PR: proper Py_XDECREF() has to be called in the proper place
+  // FIXME PR: capture error and move it to char* to print in LM_X
+  PyObject* jexl_module = PyImport_ImportModule("pyjexl");
+  LM_T(LmtJexl, ("jexl module has been loaded"));
+
+  // Create JEXL engine
+  PyObject* jexl_engine = PyObject_CallMethod(jexl_module, "JEXL", NULL);
+  LM_T(LmtJexl, ("jexl engine has been created"));
+
+  // Create expression and context
+  PyObject* expression = Py_BuildValue("s", "x + y");
+  LM_T(LmtJexl, ("jexl expression has been built"));
+  PyObject* context = Py_BuildValue("{s:i, s:i}", "x", 4, "y", 11);
+  LM_T(LmtJexl, ("jexl context has been built"));
+
+  // Call evaluate method
+  PyObject* result = PyObject_CallMethod(jexl_engine, "evaluate", "OO", expression, context);
+  LM_T(LmtJexl, ("jexl evaluation is done"));
+
+  // Print result
+  PyObject* repr = PyObject_Repr(result);
+  const char* result_str = PyUnicode_AsUTF8(repr);
+  LM_T(LmtJexl, ("jexl evaluation result is obtainted"));
+  LM_T(LmtJexl, ("jexl result: %s", result_str));
+
+  // Free resources
+  Py_XDECREF(repr);
+  Py_XDECREF(result);
+  Py_XDECREF(context);
+  Py_XDECREF(expression);
+  Py_XDECREF(jexl_engine);
+  Py_XDECREF(jexl_module);
+  LM_T(LmtJexl, ("all has been freed"));
+
+  // Finalize the Python interpreter
+  Py_Finalize();
 }
