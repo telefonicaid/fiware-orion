a6e284a7a12d55fd5be8eb601828b894a93b9c8d
Jose Ignacio Carretero <joseignacio.carretero@gmail.com>
2024-04-12 14:16:26 +0200

    Changed tinyxml2 libraries to Alma Linux 8 and slight modification to Fast.DDS source to compile in RH8

--- docker/build-ubi/install-fastdds.sh
+++ docker/build-ubi/install-fastdds.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 # Copyright 2024 Telefonica Investigacion y Desarrollo, S.A.U
 #
@@ -20,10 +20,9 @@
 # For those usages not covered by this license please contact with
 # iot_support at tid dot es
 
-# yum -y --nogpgcheck install https://dl.rockylinux.org/pub/rocky/9/devel/x86_64/os/Packages/t/tinyxml2-devel-7.0.1-7.el9.x86_64.rpm
-# yum -y --nogpgcheck install https://dl.rockylinux.org/pub/rocky/9/devel/x86_64/os/Packages/a/asio-devel-1.16.1-6.el9.x86_64.rpm
+yum -y --nogpgcheck install https://repo.almalinux.org/almalinux/8/PowerTools/x86_64/os/Packages/tinyxml2-6.0.0-3.el8.x86_64.rpm \
+          https://repo.almalinux.org/almalinux/8/PowerTools/x86_64/os/Packages/tinyxml2-devel-6.0.0-3.el8.x86_64.rpm 
 
-yum -y --nogpgcheck install https://dl.fedoraproject.org/pub/fedora/linux/releases/39/Everything/x86_64/os/Packages/t/tinyxml2-devel-9.0.0-3.fc39.x86_64.rpm
 yum -y --nogpgcheck install https://dl.fedoraproject.org/pub/fedora/linux/releases/39/Everything/x86_64/os/Packages/a/asio-devel-1.28.1-2.fc39.x86_64.rpm
 
 mkdir /opt/Fast-DDS
@@ -35,6 +34,7 @@ cd /opt/Fast-DDS
 git clone https://github.com/eProsima/foonathan_memory_vendor.git
 mkdir foonathan_memory_vendor/build
 cd foonathan_memory_vendor/build
+
 cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local/ -DBUILD_SHARED_LIBS=ON
 cmake --build . --target install
 cd -
@@ -54,6 +54,14 @@ cd -
 #
 git clone https://github.com/eProsima/Fast-DDS.git
 mkdir Fast-DDS/build
+
+
+## Prevent glibc bug: https://stackoverflow.com/questions/30680550/c-gettid-was-not-declared-in-this-scope
+file_bug="/opt/Fast-DDS/Fast-DDS/src/cpp/utils/threading/threading_pthread.ipp"
+
+nl=$(grep -n "namespace eprosima" $file_bug | awk -F':' '{print $1 ; exit 0}')
+sed -i "${nl}i #include <unistd.h>\n#include <sys/syscall.h>\n#define gettid() syscall(SYS_gettid)\n" $file_bug
+
 
 cd Fast-DDS/build
 cmake ..  -DCMAKE_INSTALL_PREFIX=/usr/local/ -DBUILD_SHARED_LIBS=ON
