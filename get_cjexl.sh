ba0826f3add3e57c6104717357dd6086df84e3b7
Fermín Galán Márquez <fermin.galanmarquez@telefonica.com>
2024-05-28 13:01:31 +0200

    FIX improve get_cjexl.sh

--- get_cjexl.sh
+++ get_cjexl.sh
@@ -23,7 +23,7 @@ TOKEN=$2
 
 res_code=$(curl -I -s -o /dev/null -w "%{http_code}" -H "Authorization: token $TOKEN" https://api.github.com/repos/telefonicasc/cjexl/releases/tags/$CJEXL_VERSION)
 if [ "$res_code" -eq 200 ]; then
-    echo "get_cjexl: downloading cjexl lib"
+    echo "get_cjexl: downloading cjexl lib $CJEXL_VERSION"
     ASSET_ID=$(curl -s -S -H "Authorization: token $TOKEN" https://api.github.com/repos/telefonicasc/cjexl/releases/tags/$CJEXL_VERSION | grep -C3 libcjexl.a | grep '"id"' | awk -F ' ' '{print $2}' | awk -F ',' '{print $1}')
     curl -L -s -o /usr/local/lib/libcjexl.a -H "Authorization: token $TOKEN" -H "Accept: application/octet-stream" https://api.github.com/repos/telefonicasc/cjexl/releases/assets/$ASSET_ID
     MD5SUM=$(md5sum /usr/local/lib/libcjexl.a | awk -F ' ' '{print$1}')
