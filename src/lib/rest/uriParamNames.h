5b9c5791b8ebff5cd874425ea915e10892d4576f
Fermín Galán Márquez <fermin.galanmarquez@telefonica.com>
2025-07-29 17:57:20 +0200

    REMOVE unused URL params

--- src/lib/rest/uriParamNames.h
+++ src/lib/rest/uriParamNames.h
@@ -35,8 +35,6 @@
 */
 #define URI_PARAM_PAGINATION_OFFSET       "offset"
 #define URI_PARAM_PAGINATION_LIMIT        "limit"
-#define URI_PARAM_ATTRIBUTES_FORMAT       "attributesFormat"
-#define URI_PARAM_ATTRIBUTE_FORMAT        "attributeFormat"
 #define URI_PARAM_OPTIONS                 "options"
 #define URI_PARAM_TYPE                    "type"
 #define URI_PARAM_SORTED                  "orderBy"
